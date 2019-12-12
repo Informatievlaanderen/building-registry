13ad30533c84b8fcf1252ae4a5fdfd1dab3cfa21
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-12-12 14:43:51 +0100

    perf: add index on buildingunitdetail projection

---
+++ src/BuildingRegistry.Projections.Legacy/Migrations/20191212134319_AddBuildingIndexOnBuildingUnitDetail.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildingRegistry.Projections.Legacy.Migrations
+{
+    public partial class AddBuildingIndexOnBuildingUnitDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BuildingUnitDetails_BuildingId",
+                schema: "BuildingRegistryLegacy",
+                table: "BuildingUnitDetails",
+                column: "BuildingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BuildingUnitDetails_BuildingId",
+                schema: "BuildingRegistryLegacy",
+                table: "BuildingUnitDetails");
+        }
+    }
+}
