ef170a2fb29ea1c8e4b6748c276b9fd1468bfd83
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-01-31 12:45:24 +0100

    perf: add index on IsComplete filter for views WMS

---
+++ src/BuildingRegistry.Projections.Wms/Migrations/20200131114305_AddIsCompleteIndex.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuildingRegistry.Projections.Wms.Migrations
+{
+    public partial class AddIsCompleteIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BuildingUnits_IsComplete_IsBuildingComplete",
+                schema: "wms",
+                table: "BuildingUnits",
+                columns: new[] { "IsComplete", "IsBuildingComplete" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_IsComplete",
+                schema: "wms",
+                table: "Buildings",
+                column: "IsComplete");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BuildingUnits_IsComplete_IsBuildingComplete",
+                schema: "wms",
+                table: "BuildingUnits");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Buildings_IsComplete",
+                schema: "wms",
+                table: "Buildings");
+        }
+    }
+}
