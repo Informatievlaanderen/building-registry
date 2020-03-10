3a00be91050803a956efb16be9d6b288f9e00935
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-03-10 13:22:49 +0100

    feat: add totaal aantal endpoints

---
+++ src/BuildingRegistry.Api.Legacy/Infrastructure/TotalCountResponseExample.cs
@@ -0,0 +1,16 @@
+namespace BuildingRegistry.Api.Legacy.Infrastructure
+{
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy;
+    using Swashbuckle.AspNetCore.Filters;
+
+    public class TotalCountResponseExample : IExamplesProvider<TotaalAantalResponse>
+    {
+        public TotaalAantalResponse GetExamples()
+        {
+            return new TotaalAantalResponse
+            {
+                Aantal = 574512
+            };
+        }
+    }
+}
