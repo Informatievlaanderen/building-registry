60a5c1926ef301413a960739f9469b54fd923060
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-05-22 15:58:36 +0200

    refactor: use constants for extract filenames GRAR-356

--- src/BuildingRegistry.Api.Extract/Extracts/ExtractFileNames.cs
+++ src/BuildingRegistry.Api.Extract/Extracts/ExtractFileNames.cs
@@ -4,8 +4,8 @@ namespace BuildingRegistry.Api.Extract.Extracts
 
     internal class ExtractFileNames
     {
-        public static string Building => "Gebouw";
-        public static string BuildingUnit => "Gebouweenheid";
+        public const string Building = "Gebouw";
+        public const string BuildingUnit = "Gebouweenheid";
 
         public static string GetBuildingZipName() => $"Gebouw-{DateTime.Today:yyyy-MM-dd}";
     }
