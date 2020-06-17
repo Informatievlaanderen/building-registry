0f8dbce2923198a6365a986033f489bc685552c5
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-06-17 17:27:27 +0200

    fix: provenance for fix-1359 is now added

--- src/BuildingRegistry/BuildingProvenanceFactory.cs
+++ src/BuildingRegistry/BuildingProvenanceFactory.cs
@@ -2,6 +2,8 @@ namespace BuildingRegistry
 {
     using System;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using Building.Commands.Crab;
+    using NodaTime;
 
     public class BuildingProvenanceFactory : CrabProvenanceFactory, IProvenanceFactory<Building.Building>
     {
@@ -19,6 +21,20 @@ namespace BuildingRegistry
                 crabProvenance.Modification,
                 crabProvenance.Operator,
                 crabProvenance.Organisation);
+        }
+    }
+
+    public class FixGrar1359ProvenanceFactory : CrabProvenanceFactory, IProvenanceFactory<Building.Building>
+    {
+        public bool CanCreateFrom<TCommand>() => typeof(FixGrar1359).IsAssignableFrom(typeof(TCommand));
+
+        public Provenance CreateFrom(object provenanceHolder,
+            Building.Building aggregate)
+        {
+            if (!(provenanceHolder is FixGrar1359 crabProvenance))
+                throw new ApplicationException($"Cannot create provenance from {provenanceHolder.GetType().Name}");
+
+            return new Provenance(Instant.FromDateTimeUtc(DateTime.UtcNow), Application.Unknown, new Reason("Rechtzetting gebouweenheden"), new Operator("crabadmin"), Modification.Update, Organisation.Aiv);
         }
     }
 }
