c138fdaabc13c8235b806a55e5fccd1f591f349e
David Cumps <david@cumps.be>
2020-06-23 12:44:40 +0200

    fix: configure baseurls for all problemdetails GRAR-1357

--- src/BuildingRegistry.Projector/Infrastructure/Startup.cs
+++ src/BuildingRegistry.Projector/Infrastructure/Startup.cs
@@ -46,6 +46,11 @@ namespace BuildingRegistry.Projector.Infrastructure
         /// <param name="services">The collection of services to configure the application with.</param>
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            var baseUrl = _configuration.GetValue<string>("BaseUrl");
+            var baseUrlForExceptions = baseUrl.EndsWith("/")
+                ? baseUrl.Substring(0, baseUrl.Length - 1)
+                : baseUrl;
+
             services
                 .ConfigureDefaultForApi<Startup>(new StartupConfigureOptions
                 {
@@ -56,6 +61,10 @@ namespace BuildingRegistry.Projector.Infrastructure
                             .GetChildren()
                             .Select(c => c.Value)
                             .ToArray()
+                    },
+                    Server =
+                    {
+                        BaseUrl = baseUrlForExceptions
                     },
                     Swagger =
                     {
