f2e9d1537556a611bccd0c44aa20670b64f120eb
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-09-15 08:55:58 +0200

    fix: correct applying provenance on readdress event GRAR-1548

--- src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasReaddressed.cs
+++ src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasReaddressed.cs
@@ -3,18 +3,21 @@ namespace BuildingRegistry.Building.Events
     using Be.Vlaanderen.Basisregisters.EventHandling;
     using Newtonsoft.Json;
     using System;
+    using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
     using NodaTime;
     using ValueObjects;
 
     [EventName("BuildingUnitWasReaddressed")]
     [EventDescription("Gebouweenheid werd geheradresseerd")]
-    public class BuildingUnitWasReaddressed
+    public class BuildingUnitWasReaddressed : IHasProvenance, ISetProvenance
     {
         public Guid BuildingId { get; }
         public Guid BuildingUnitId { get; }
         public Guid OldAddressId { get; }
         public Guid NewAddressId { get; }
         public LocalDate BeginDate { get; }
+
+        public ProvenanceData Provenance { get; private set; }
 
         public BuildingUnitWasReaddressed(
             BuildingId buildingId,
@@ -36,12 +39,16 @@ namespace BuildingRegistry.Building.Events
             Guid buildingUnitId,
             Guid oldAddressId,
             Guid newAddressId,
-            LocalDate beginDate)
+            LocalDate beginDate,
+            ProvenanceData provenance)
             : this(
                 new BuildingId(buildingId),
                 new BuildingUnitId(buildingUnitId),
                 new AddressId(oldAddressId),
                 new AddressId(newAddressId),
-                new ReaddressingBeginDate(beginDate)) { }
+                new ReaddressingBeginDate(beginDate))
+                    => ((ISetProvenance)this).SetProvenance(provenance.ToProvenance());
+
+        void ISetProvenance.SetProvenance(Provenance provenance) => Provenance = new ProvenanceData(provenance);
     }
 }
