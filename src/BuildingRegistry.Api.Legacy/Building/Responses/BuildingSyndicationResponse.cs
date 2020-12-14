031780ce92af4f766def89130d153d9c88ac7813
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-12-14 10:07:03 +0100

    fix: rename adressen to adressenids in sync

--- src/BuildingRegistry.Api.Legacy/Building/Responses/BuildingSyndicationResponse.cs
+++ src/BuildingRegistry.Api.Legacy/Building/Responses/BuildingSyndicationResponse.cs
@@ -267,7 +267,7 @@ namespace BuildingRegistry.Api.Legacy.Building.Responses
         /// <summary>
         /// De gebouweenheden van de gebouweenheid.
         /// </summary>
-        [DataMember(Name = "Adressen", Order = 7)]
+        [DataMember(Name = "AdressenIds", Order = 7)]
         public List<Guid> Addresses { get; set; }
 
         /// <summary>
