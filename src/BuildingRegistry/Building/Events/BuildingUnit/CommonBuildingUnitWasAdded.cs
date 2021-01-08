e8e1f53c6ee6f83e62a73fda491aa068dd7ca77c
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-08 18:18:44 +0100

    docs: changed EventDescription/added EventPropertyDescriptions GRAR-1618

--- src/BuildingRegistry/Building/Events/BuildingUnit/CommonBuildingUnitWasAdded.cs
+++ src/BuildingRegistry/Building/Events/BuildingUnit/CommonBuildingUnitWasAdded.cs
@@ -8,13 +8,22 @@ namespace BuildingRegistry.Building.Events
     using ValueObjects;
 
     [EventName("CommonBuildingUnitWasAdded")]
-    [EventDescription("Gemeenschappelijk gebouweenheid werd toegevoegd.")]
+    [EventDescription("De gebouweenheid met functie 'gemeenschappelijk deel' werd toegevoegd.")]
     public class CommonBuildingUnitWasAdded : IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van het gebouw waartoe de gebouweenheid behoort.")]
         public Guid BuildingId { get; }
+        
+        [EventPropertyDescription("Interne GUID van de gebouweenheid.")]
         public Guid BuildingUnitId { get; }
+        
+        [EventPropertyDescription("Informatie in CRAB waarop de aanmaak van de gebouweenheid gebaseerd werd.")]
         public BuildingUnitKeyType BuildingUnitKey { get; }
+        
+        [EventPropertyDescription("Versie van de gebouweenheid.")]
         public Instant BuildingUnitVersion { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public CommonBuildingUnitWasAdded(
