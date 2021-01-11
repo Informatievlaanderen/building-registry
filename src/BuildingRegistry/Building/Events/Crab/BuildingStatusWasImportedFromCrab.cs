e4f90ddff1c23571995a82ce0ac1c8cab0d6c2b8
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-11 12:17:10 +0100

    docs: added EventPropertyDescriptions GRAR-1618

--- src/BuildingRegistry/Building/Events/Crab/BuildingStatusWasImportedFromCrab.cs
+++ src/BuildingRegistry/Building/Events/Crab/BuildingStatusWasImportedFromCrab.cs
@@ -10,16 +10,34 @@ namespace BuildingRegistry.Building.Events.Crab
     [EventDescription("Legacy event om tblgebouwstatus en tblgebouwstatus_hist te importeren.")]
     public class BuildingStatusWasImportedFromCrab : ICrabEvent, IHasCrabKey<int>
     {
+        [EventPropertyDescription("CRAB-identificator van de gebouwstatus.")]
         public int BuildingStatusId { get; }
+        
+        [EventPropertyDescription("CRAB-identificator van het terreinobject.")]
         public int TerrainObjectId { get; }
+        
+        [EventPropertyDescription("Gebouwstatus.")]
         public CrabBuildingStatus BuildingStatus { get; }
+        
+        [EventPropertyDescription("Datum waarop het object is ontstaan in werkelijkheid.")]
         public LocalDateTime? BeginDateTime { get; }
+        
+        [EventPropertyDescription("Datum waarop het object in werkelijkheid ophoudt te bestaan.")]
         public LocalDateTime? EndDateTime { get; }
+        
+        [EventPropertyDescription("Tijdstip waarop het object werd ingevoerd in de databank.")]
         public Instant Timestamp { get; }
+        
+        [EventPropertyDescription("Operator door wie het object werd ingevoerd in de databank.")]
         public string Operator { get; }
+        
+        [EventPropertyDescription("Bewerking waarmee het object werd ingevoerd in de databank.")] 
         public CrabModification? Modification { get; }
+        
+        [EventPropertyDescription("Organisatie die het object heeft ingevoerd in de databank.")]
         public CrabOrganisation? Organisation { get; }
 
+        [EventPropertyDescription("Unieke sleutel.")]
         public int Key => BuildingStatusId;
 
         public BuildingStatusWasImportedFromCrab(
