5cd3b83e833ab5154b3dfaf2f121c6b36c418291
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-11 09:55:49 +0100

    docs: changed EventDescription/added EventPropertyDescriptions GRAR-1618

--- src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasReaddedByOtherUnitRemoval.cs
+++ src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasReaddedByOtherUnitRemoval.cs
@@ -8,16 +8,28 @@ namespace BuildingRegistry.Building.Events
     using ValueObjects;
 
     [EventName("BuildingUnitWasReaddedByOtherUnitRemoval")]
-    [EventDescription(
-        "Gebouweenheid werd opnieuw toegevoegd door het verwijderen/historeren van andere gebouweenheid.")]
+    [EventDescription("De gebouweenheid werd opnieuw toegevoegd door het verwijderen/historeren van een andere gebouweenheid.")]
     public class BuildingUnitWasReaddedByOtherUnitRemoval : IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van het gebouw waartoe de gebouweenheid behoort.")]
         public Guid BuildingId { get; }
+        
+        [EventPropertyDescription("Interne GUID van de gebouweenheid.")]
         public Guid BuildingUnitId { get; }
+        
+        [EventPropertyDescription("Informatie in CRAB waarop de aanmaak van de gebouweenheid gebaseerd werd.")]
         public BuildingUnitKeyType BuildingUnitKey { get; }
+        
+        [EventPropertyDescription("Interne GUID van het adres dat aan de gebouweenheid werd gekoppeld.")]
         public Guid AddressId { get; }
+        
+        [EventPropertyDescription("Versie van de gebouweenheid.")]
         public Instant BuildingUnitVersion { get; }
+        
+        [EventPropertyDescription("Interne GUID van een eerdere gebouweenheid waarop deze gebouweenheid bij de aanmaak gebaseerd werd.")]
         public Guid PredecessorBuildingUnitId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public BuildingUnitWasReaddedByOtherUnitRemoval(
