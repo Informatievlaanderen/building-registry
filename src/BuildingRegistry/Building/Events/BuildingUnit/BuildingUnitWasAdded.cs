9d85b57ca9f1a810d943fe803117b7d272a03388
rubcap <26146350+rubcap@users.noreply.github.com>
2021-01-11 10:12:44 +0100

    docs: corrected EventDescription "BuildingUnitKeyType" GRAR-1618

--- src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasAdded.cs
+++ src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasAdded.cs
@@ -17,7 +17,7 @@ namespace BuildingRegistry.Building.Events
         [EventPropertyDescription("Interne GUID van de gebouweenheid.")]
         public Guid BuildingUnitId { get; }
         
-        [EventPropertyDescription("Informatie in CRAB waarop de aanmaak van de gebouweenheid gebaseerd werd.")]
+        [EventPropertyDescription("Informatie waarop de aanmaak van de gebouweenheid gebaseerd werd.")]
         public BuildingUnitKeyType BuildingUnitKey { get; }
         
         [EventPropertyDescription("Interne GUID van het adres dat aan de gebouweenheid werd gekoppeld.")]
