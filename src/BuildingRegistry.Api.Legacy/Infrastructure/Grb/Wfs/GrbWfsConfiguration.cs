66f8768355c8627656aecea84ce4ba3e733655ba
David Cumps <david@cumps.be>
2021-01-20 23:53:21 +0100

    style: props above ctors

--- src/BuildingRegistry.Api.Legacy/Infrastructure/Grb/Wfs/GrbWfsConfiguration.cs
+++ src/BuildingRegistry.Api.Legacy/Infrastructure/Grb/Wfs/GrbWfsConfiguration.cs
@@ -4,11 +4,11 @@ namespace BuildingRegistry.Api.Legacy.Infrastructure.Grb.Wfs
 
     internal class GrbWfsConfiguration
     {
+        public string Url { get; }
+        
         public GrbWfsConfiguration(string url)
             => Url = !string.IsNullOrWhiteSpace(url)
                 ? url
                 : throw new ArgumentNullException(nameof(url));
-
-        public string Url { get; }
     }
 }
