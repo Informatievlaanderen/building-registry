1f265d361e78a334ac1389c85b013fe13336022f
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2021-01-21 16:42:41 +0100

    fix: add handler for GRB WFS exceptions GRAR-170

---
+++ src/BuildingRegistry.Api.Legacy/Infrastructure/Grb/Wfs/GrbWfsExceptionHandler.cs
@@ -0,0 +1,18 @@
+namespace BuildingRegistry.Api.Legacy.Infrastructure.Grb.Wfs
+{
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.BasicApiProblem;
+    using Microsoft.AspNetCore.Http;
+
+    internal class GrbWfsExceptionHandler : DefaultExceptionHandler<GrbWfsException>
+    {
+        protected override ProblemDetails GetApiProblemFor(GrbWfsException exception)
+            => new ProblemDetails
+            {
+                HttpStatus = StatusCodes.Status500InternalServerError,
+                Title = ProblemDetails.DefaultTitle,
+                Detail = exception.Message,
+                ProblemTypeUri = ProblemDetails.GetTypeUriFor(exception)
+            };
+    }
+}
