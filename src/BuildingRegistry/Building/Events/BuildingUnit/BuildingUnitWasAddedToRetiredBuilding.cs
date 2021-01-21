e956f0470e565a1ee72d899660276ed6e9e871ba
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2021-01-21 10:15:07 +0100

    fix: correct usage of address in BuildingUnitWasAddedToRetiredBuilding GRAR-1767

--- src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasAddedToRetiredBuilding.cs
+++ src/BuildingRegistry/Building/Events/BuildingUnit/BuildingUnitWasAddedToRetiredBuilding.cs
@@ -13,22 +13,22 @@ namespace BuildingRegistry.Building.Events
     {
         [EventPropertyDescription("Interne GUID van het gebouw waartoe de gebouweenheid behoort.")]
         public Guid BuildingId { get; }
-        
+
         [EventPropertyDescription("Interne GUID van de gebouweenheid.")]
         public Guid BuildingUnitId { get; }
-        
+
         [EventPropertyDescription("Informatie waarop de aanmaak van de gebouweenheid gebaseerd werd.")]
         public BuildingUnitKeyType BuildingUnitKey { get; }
-        
+
         [EventPropertyDescription("Interne GUID van het adres dat aan de gebouweenheid werd gekoppeld.")]
         public Guid AddressId { get; }
-        
+
         [EventPropertyDescription("Versie van de gebouweenheid.")]
         public Instant BuildingUnitVersion { get; }
-        
+
         [EventPropertyDescription("Interne GUID van een eerdere gebouweenheid waarop deze gebouweenheid bij de aanmaak gebaseerd werd.")]
         public Guid? PredecessorBuildingUnitId { get; }
-        
+
         [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
@@ -76,7 +76,7 @@ namespace BuildingRegistry.Building.Events
                 new AddressId(addressId),
                 new BuildingUnitVersion(buildingUnitVersion),
                 predecessorBuildingUnitId.HasValue ? new BuildingUnitId(predecessorBuildingUnitId.Value) : null) => ((ISetProvenance)this).SetProvenance(provenance.ToProvenance());
-        
+
         void ISetProvenance.SetProvenance(Provenance provenance) => Provenance = new ProvenanceData(provenance);
     }
 }
