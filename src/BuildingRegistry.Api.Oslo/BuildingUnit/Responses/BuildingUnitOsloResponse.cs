0b49a62e686113db2c6198a57c87b801c666af3b
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2021-12-13 15:15:52 +0100

    fix: add gml property to BuildingUnitOsloResponse

--- src/BuildingRegistry.Api.Oslo/BuildingUnit/Responses/BuildingUnitOsloResponse.cs
+++ src/BuildingRegistry.Api.Oslo/BuildingUnit/Responses/BuildingUnitOsloResponse.cs
@@ -28,16 +28,16 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.Responses
 	""id"": ""@id"",
 	""objectId"":""http://www.w3.org/2001/XMLSchema#string"",
  	""detail"": ""http://www.iana.org/assignments/relation/self"",
-	""versieId"": {        
-		""@id"": ""https://data.vlaanderen.be/ns/generiek#versieIdentificator"",        
-		""@type"": ""http://www.w3.org/2001/XMLSchema#string""}, 
+	""versieId"": {
+		""@id"": ""https://data.vlaanderen.be/ns/generiek#versieIdentificator"",
+		""@type"": ""http://www.w3.org/2001/XMLSchema#string""},
 	""geometriePunt"": {
 		""@id"" : ""https://data.vlaanderen.be/ns/gebouw#Gebouweenheid.geometrie"",
 		""@type"": ""@id"",
 		""@context"":{
 			""point"": ""http://www.opengis.net/ont/sf#Point"",
 			""type"": ""@type""
-		
+
 		}
 	},
 	""positieGeometrieMethode"":""https://data.vlaanderen.be/id/conceptscheme/geometriemethode"",
@@ -55,7 +55,6 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.Responses
 			""@base"":""https://data.vlaanderen.be/doc/concept/gebouweenheidsfunction/""
 		}
 	}
-	
 }";
 
         /// <summary>
@@ -73,44 +72,37 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.Responses
         public GebouweenheidIdentificator Identificator { get; set; }
 
         /// <summary>
-        /// De puntgeometrie van de gebouweenheid in gml- of geoJSON-formaat, afhankelijk van het content-type.
+        /// De puntgeometrie van de gebouweenheid in gml3.
         /// </summary>
-        [DataMember(Name = "GeometriePunt", Order = 3)]
+        [DataMember(Name = "GebouweenheidPositie", Order = 3)]
         [JsonProperty(Required = Required.DisallowNull)]
-        public Point Geometry { get; set; }
+        public BuildingUnitPosition BuildingUnitPosition { get; set; }
 
         /// <summary>
-        /// De geometriemethode van de gebouweenheidpositie. 
+        /// De status van de gebouweenheid.
         /// </summary>
-        [DataMember(Name = "PositieGeometrieMethode", Order = 4)]
-        [JsonProperty(Required = Required.DisallowNull)]
-        public PositieGeometrieMethode GeometryMethod { get; set; }
-
-        /// <summary>
-        /// De status van de gebouweenheid. 
-        /// </summary>
-        [DataMember(Name = "GebouweenheidStatus", Order = 5)]
+        [DataMember(Name = "GebouweenheidStatus", Order = 4)]
         [JsonProperty(Required = Required.DisallowNull)]
         public GebouweenheidStatus Status { get; set; }
 
         /// <summary>
         /// the function of the building unit in reality (as observed on site)
         /// </summary>
-        [DataMember(Name = "Functie", Order = 6)]
+        [DataMember(Name = "Functie", Order = 5)]
         [JsonProperty(Required = Required.DisallowNull)]
         public GebouweenheidFunctie? Function { get; set; }
 
         /// <summary>
         /// building wherein the building unit resides
         /// </summary>
-        [DataMember(Name = "Gebouw", Order = 7)]
+        [DataMember(Name = "Gebouw", Order = 6)]
         [JsonProperty(Required = Required.DisallowNull)]
         public GebouweenheidDetailGebouw Building { get; set; }
 
         /// <summary>
         /// De aan de gebouweenheid gekoppelde adressen.
         /// </summary>
-        [DataMember(Name = "Adressen", Order = 8)]
+        [DataMember(Name = "Adressen", Order = 7)]
         [JsonProperty(Required = Required.DisallowNull)]
         public List<GebouweenheidDetailAdres> Addresses { get; set; }
 
@@ -118,16 +110,14 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.Responses
             int persistentLocalId,
             string naamruimte,
             DateTimeOffset version,
-            Point geometry,
-            PositieGeometrieMethode geometryMethod,
+            BuildingUnitPosition buildingUnitPosition,
             GebouweenheidStatus status,
             GebouweenheidFunctie? function,
             GebouweenheidDetailGebouw building,
             List<GebouweenheidDetailAdres> addresses)
         {
             Identificator = new GebouweenheidIdentificator(naamruimte, persistentLocalId.ToString(), version);
-            Geometry = geometry;
-            GeometryMethod = geometryMethod;
+            BuildingUnitPosition = buildingUnitPosition;
             Status = status;
             Function = function;
             Building = building;
@@ -147,19 +137,9 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.Responses
                 6,
                 _responseOptions.GebouweenheidNaamruimte,
                 DateTimeOffset.Now.ToExampleOffset(),
-                new Point
-                {
-                    JsonPoint = new GeoJSONPoint
-                    {
-                        Type = "point",
-                        Coordinates = new[] { 140252.76, 198794.27 }
-                    },
-                    XmlPoint = new GmlPoint
-                    {
-                        Pos = "140252.76 198794.27"
-                    }
-                },
-                PositieGeometrieMethode.AangeduidDoorBeheerder,
+                new BuildingUnitPosition(
+                    new GmlJsonPoint("<gml:Point srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:pos>140252.76 198794.27</gml:pos></gml:Point>"),
+                    PositieGeometrieMethode.AangeduidDoorBeheerder),
                 GebouweenheidStatus.Gerealiseerd,
                 GebouweenheidFunctie.GemeenschappelijkDeel,
                 new GebouweenheidDetailGebouw("1", string.Format(_responseOptions.GebouwDetailUrl,"1")),
