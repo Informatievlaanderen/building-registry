ef36f5ba802a337b67efe514a5c3fcdfcb86b0f9
Mavzer Yusuf <yusuf.mavzer1@gmail.com>
2022-01-04 11:23:01 +0100

    feat: GAWR-2519 add buildingPolygoon property with gml 3.2 xml string

---
+++ src/BuildingRegistry.Api.Oslo/Building/Responses/BuildingPolygon.cs
@@ -0,0 +1,35 @@
+namespace BuildingRegistry.Api.Oslo.Building.Responses
+{
+    using System.Runtime.Serialization;
+    using System.Xml.Serialization;
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy.Gebouw;
+    using Be.Vlaanderen.Basisregisters.GrAr.Legacy.SpatialTools;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// De geometrie van het object in gml-formaat.
+    /// </summary>
+    [DataContract(Name = "GebouwPolygoon", Namespace = "")]
+    public class BuildingPolygon
+    {
+        /// <summary>
+        /// De geometrie.
+        /// </summary>
+        [JsonProperty("geometrie")]
+        [XmlIgnore]
+        public GmlJsonPolygon Geometry { get; set; }
+
+        /// <summary>
+        /// De gebruikte methode om de positie te bepalen.
+        /// </summary>
+        [DataMember(Name = "GeometrieMethode", Order = 1)]
+        [JsonProperty(Required = Required.DisallowNull)]
+        public GeometrieMethode GeometryMethod { get; set; }
+
+        public BuildingPolygon(GmlJsonPolygon geometry, GeometrieMethode geometryMethod)
+        {
+            Geometry = geometry;
+            GeometryMethod = geometryMethod;
+        }
+    }
+}
