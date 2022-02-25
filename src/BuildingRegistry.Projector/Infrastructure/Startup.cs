89b55c743aff33a95d683e428908e419e595f407
pgallik <patrickgallik@gmail.com>
2022-02-25 13:54:07 +0100

    feat: update api to 17.0.0

--- src/BuildingRegistry.Projector/Infrastructure/Startup.cs
+++ src/BuildingRegistry.Projector/Infrastructure/Startup.cs
@@ -145,7 +145,7 @@ namespace BuildingRegistry.Projector.Infrastructure
             ApiDebugDataDogToggle debugDataDogToggle,
             HealthCheckService healthCheckService)
         {
-            StartupHelpers.CheckDatabases(healthCheckService, DatabaseTag).GetAwaiter().GetResult();
+            StartupHelpers.CheckDatabases(healthCheckService, DatabaseTag, loggerFactory).GetAwaiter().GetResult();
 
             app
                 .UseDataDog<Startup>(new DataDogOptions
