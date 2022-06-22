1cc21adb0185c3c19c738b3eef52136a6f55aef6
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-06-22 16:25:28 +0200

    feat: add backoffice handlers sqs project (to be implemented)

---
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Requests/SqsRealizeBuildingRequest.cs
@@ -0,0 +1,28 @@
+namespace BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using BuildingRegistry.Building;
+    using BuildingRegistry.Building.Commands;
+    using MediatR;
+    using Newtonsoft.Json;
+
+    [DataContract(Name = "RealiseerGebouw", Namespace = "")]
+    public class SqsRealizeBuildingRequest : IRequest<Unit>
+    {
+        public int PersistentLocalId { get; set; }
+
+        [JsonIgnore]
+        public IDictionary<string, object> Metadata { get; set; }
+
+        public RealizeBuilding ToCommand(
+            BuildingPersistentLocalId buildingPersistentLocalId,
+            Provenance provenance)
+        {
+            return new RealizeBuilding(
+                buildingPersistentLocalId,
+                provenance);
+        }
+    }
+}
---
+++ src/BuildingRegistry.Api.BackOffice.Handlers.Sqs/Building/SqsRealizeBuildingHandler.cs
@@ -0,0 +1,49 @@
+namespace BuildingRegistry.Api.BackOffice.Handlers.Sqs.Building
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.CommandHandling;
+    using Be.Vlaanderen.Basisregisters.CommandHandling.Idempotency;
+    using BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests;
+    using BuildingRegistry.Building;
+    using MediatR;
+
+    public class SqsRealizeBuildingHandler : SqsBusHandler, IRequestHandler<SqsRealizeBuildingRequest, Unit>
+    {
+        private readonly IdempotencyContext _idempotencyContext;
+        private readonly IBuildings _buildings;
+
+        public SqsRealizeBuildingHandler(
+            ICommandHandlerResolver bus,
+            IdempotencyContext idempotencyContext,
+            IBuildings buildings) : base(bus)
+        {
+            _idempotencyContext = idempotencyContext;
+            _buildings = buildings;
+        }
+
+        public async Task<Unit> Handle(SqsRealizeBuildingRequest request, CancellationToken cancellationToken)
+        {
+            var buildingPersistentLocalId = new BuildingPersistentLocalId(request.PersistentLocalId);
+
+            var planBuilding = request.ToCommand(
+                buildingPersistentLocalId,
+                CreateFakeProvenance());
+
+            await IdempotentCommandHandlerDispatch(
+                _idempotencyContext,
+                planBuilding.CreateCommandId(),
+                planBuilding,
+                request.Metadata,
+                cancellationToken);
+
+            var buildingHash = await GetBuildingHash(
+                _buildings,
+                buildingPersistentLocalId,
+                cancellationToken);
+
+            return Unit.Value;
+            //return new ETagResponse(buildingHash);
+        }
+    }
+}
