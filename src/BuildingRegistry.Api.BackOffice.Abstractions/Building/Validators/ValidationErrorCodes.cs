f412543b7b7754fd1e61f4cc25cf71cf8359766d
Arne Dumarey <dumarey.arne@gmail.com>
2022-06-23 16:37:57 +0200

    fix: correct errorcodes realize/underconstruction

--- src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrorCodes.cs
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrorCodes.cs
@@ -3,7 +3,7 @@ namespace BuildingRegistry.Api.BackOffice.Abstractions.Building.Validators
     public static class ValidationErrorCodes
     {
         public static string InvalidPolygonGeometry = "GebouwPolygoonValidatie";
-        public static string BuildingCannotBePlacedUnderConstruction = "GebouwStatusNietGepland";
-        public static string BuildingCannotBeRealizedException = "GebouwStatusInAanbouw";
+        public static string BuildingCannotBePlacedUnderConstruction = "GebouwGehistoreerdGerealiseerdOfNietGerealiseerd";
+        public static string BuildingCannotBeRealizedException = "GebouwGehistoreerdGeplandOfNietGerealiseerd";
     }
 }
