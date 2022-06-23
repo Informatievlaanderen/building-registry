f9f992b8074fe66af4a4b0ed89145e024ecb7a90
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-06-23 12:44:12 +0200

    feat: add backoffice api lambda

---
+++ src/BuildingRegistry.Api.BackOffice.Handlers.Sqs.Lambda/SqsBackOfficeHandlerFunction.cs
@@ -0,0 +1,21 @@
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace BuildingRegistry.Api.BackOffice.Handlers.Sqs.Lambda
+{
+    using Be.Vlaanderen.Basisregisters.Aws.Lambda;
+    using Building;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class SqsBackOfficeHandlerFunction : FunctionBase
+    {
+        public override void ConfigureServices(ServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            services.AddTransient<IMessageHandler, MessageHandler>();
+            services.AddMediatR(typeof(SqsPlanBuildingHandler).Assembly);
+        }
+    }
+}
