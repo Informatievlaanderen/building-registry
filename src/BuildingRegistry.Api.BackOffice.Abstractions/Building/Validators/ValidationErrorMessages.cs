42ea36c3e44749904a78217247b012727d556633
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2022-06-23 10:27:26 +0200

    fix: use documentation for invalid polygon validation

--- src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrorMessages.cs
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrorMessages.cs
@@ -2,7 +2,7 @@ namespace BuildingRegistry.Api.BackOffice.Abstractions.Building.Validators
 {
     public static class ValidationErrorMessages
     {
-        public static string InvalidGeometry = "InvalidGeometry";
+        public static string InvalidPolygonGeometry = "Ongeldig formaat geometriePolygoon.";
         public static string BuildingRemoved = "Verwijderd gebouw.";
         public static string BuildingNotFound = "Onbestaand gebouw.";
         public static string BuildingCannotBePlacedUnderConstruction = "Deze actie is enkel toegestaan op gebouwen met status 'gepland'.";
