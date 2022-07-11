ececd8e182d8deeed9f28c1aa50f1bfb2dcd15e9
pgallik <patrickgallik@gmail.com>
2022-07-11 16:14:59 +0200

    fix: add assemblyinfo files

---
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("BuildingRegistry BackOffice Abstractions")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("28a73f9e-bda6-4f69-918c-125de08cf487")]
---
+++ src/BuildingRegistry.Api.BackOffice.Handlers.Sqs.Lambda/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("BuildingRegistry BackOffice Handlers Sqs Lambda")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c10cc82d-3240-46af-8721-b77c956ae794")]
