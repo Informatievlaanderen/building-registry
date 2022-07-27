29bc37f282248d3779dfb4dc2b253ce4134ce7b7
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-27 16:49:35 +0200

    fix: Serializable exceptions

--- src/BuildingRegistry/Legacy/BuildingRemovedException.cs
+++ src/BuildingRegistry/Legacy/BuildingRemovedException.cs
@@ -1,20 +1,23 @@
 namespace BuildingRegistry.Legacy
 {
     using System;
+    using System.Runtime.Serialization;
     using Be.Vlaanderen.Basisregisters.AggregateSource;
 
-    public class BuildingRemovedException : DomainException
+    [Serializable]
+    public sealed class BuildingRemovedException : DomainException
     {
         public BuildingRemovedException()
-        {
-        }
+        { }
+
+        private BuildingRemovedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
 
         public BuildingRemovedException(string message) : base(message)
-        {
-        }
+        { }
 
         public BuildingRemovedException(string message, Exception inner) : base(message, inner)
-        {
-        }
+        { }
     }
 }
