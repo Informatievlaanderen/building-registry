820e92ad6a769afa911dc158d4932c20ec29113d
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-27 07:53:12 +0200

    fix: finalize lambda deployment

--- src/BuildingRegistry.Api.CrabImport.Handlers.Sqs/SqsQueueName.cs
+++
@@ -1,7 +0,0 @@
-namespace BuildingRegistry.Api.CrabImport.Handlers.Sqs
-{
-    internal static class SqsQueueName
-    {
-        public const string Value = $"{nameof(BuildingRegistry)}.{nameof(Api)}.{nameof(CrabImport)}";
-    }
-}
