551fbaf5dd5a68bb37094b441b27352288932cff
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-28 11:04:42 +0200

    fix: override Equals & comparison operators

--- src/BuildingRegistry/Building/ValueObjects/ReaddressingBeginDate.cs
+++ src/BuildingRegistry/Building/ValueObjects/ReaddressingBeginDate.cs
@@ -11,6 +11,17 @@ namespace BuildingRegistry.Building
 
         public override string ToString() => Value.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
 
-        int IComparable.CompareTo(object obj) => obj is ReaddressingBeginDate date ? Value.CompareTo(date) : -1;
+        int IComparable.CompareTo(object? obj) => obj is ReaddressingBeginDate date ? Value.CompareTo(date) : -1;
+
+        public override bool Equals(object obj) => Value.Equals(obj);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public static bool operator ==(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => obj1.Equals(obj2);
+        public static bool operator !=(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => !obj1.Equals(obj2);
+        public static bool operator <(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => ((IComparable)obj1).CompareTo(obj2) < 0;
+        public static bool operator <=(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => ((IComparable)obj1).CompareTo(obj2) <= 0;
+        public static bool operator >(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => ((IComparable)obj1).CompareTo(obj2) > 0;
+        public static bool operator >=(ReaddressingBeginDate obj1, ReaddressingBeginDate obj2) => ((IComparable)obj1).CompareTo(obj2) >= 0;
     }
 }
