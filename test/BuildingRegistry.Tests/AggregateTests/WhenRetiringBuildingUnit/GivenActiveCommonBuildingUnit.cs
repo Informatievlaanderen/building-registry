4fbd1505157b4fff7ddfe5d70db64dd897c0022f
pgallik <patrickgallik@gmail.com>
2022-10-31 10:43:27 +0100

    feat: common buildingunit logic when retiring a buildingunit

---
+++ test/BuildingRegistry.Tests/AggregateTests/WhenRetiringBuildingUnit/GivenActiveCommonBuildingUnit.cs
@@ -0,0 +1,93 @@
+namespace BuildingRegistry.Tests.AggregateTests.WhenRetiringBuildingUnit
+{
+    using AutoFixture;
+    using Be.Vlaanderen.Basisregisters.AggregateSource;
+    using Be.Vlaanderen.Basisregisters.AggregateSource.Testing;
+    using Building;
+    using Building.Commands;
+    using Building.Events;
+    using Extensions;
+    using Fixtures;
+    using Xunit;
+    using Xunit.Abstractions;
+    using BuildingUnitFunction = BuildingRegistry.Legacy.BuildingUnitFunction;
+
+    public class GivenActiveCommonBuildingUnit : BuildingRegistryTest
+    {
+        public GivenActiveCommonBuildingUnit(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            Fixture.Customize(new WithFixedBuildingId());
+            Fixture.Customize(new WithFixedBuildingPersistentLocalId());
+        }
+
+        [Fact]
+        public void WithRealizedCommonBuildingUnit_ThenCommonBuildingUnitWasRetired()
+        {
+            var command = Fixture.Create<RetireBuildingUnit>();
+
+            var commonBuildingUnitPersistentLocalId = Fixture.Create<BuildingUnitPersistentLocalId>();
+
+            var buildingWasMigrated = new BuildingWasMigratedBuilder(Fixture)
+                .WithBuildingPersistentLocalId(command.BuildingPersistentLocalId)
+                .WithBuildingStatus(BuildingStatus.Realized)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    command.BuildingUnitPersistentLocalId,
+                    BuildingUnitFunction.Unknown)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    Fixture.Create<BuildingUnitPersistentLocalId>(),
+                    BuildingUnitFunction.Unknown)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    commonBuildingUnitPersistentLocalId,
+                    BuildingUnitFunction.Common)
+                .Build();
+
+            Assert(new Scenario()
+                .Given(
+                    new BuildingStreamId(Fixture.Create<BuildingPersistentLocalId>()),
+                    buildingWasMigrated)
+                .When(command)
+                .Then(
+                    new Fact(new BuildingStreamId(command.BuildingPersistentLocalId), new BuildingUnitWasRetiredV2(command.BuildingPersistentLocalId, command.BuildingUnitPersistentLocalId)),
+                    new Fact(new BuildingStreamId(command.BuildingPersistentLocalId), new BuildingUnitWasRetiredV2(command.BuildingPersistentLocalId, commonBuildingUnitPersistentLocalId))
+                ));
+        }
+
+        [Fact]
+        public void WithPlannedCommonBuildingUnit_ThenCommonBuildingUnitWasNotRealized()
+        {
+            var command = Fixture.Create<RetireBuildingUnit>();
+
+            var commonBuildingUnitPersistentLocalId = Fixture.Create<BuildingUnitPersistentLocalId>();
+
+            var buildingWasMigrated = new BuildingWasMigratedBuilder(Fixture)
+                .WithBuildingPersistentLocalId(command.BuildingPersistentLocalId)
+                .WithBuildingStatus(BuildingStatus.Realized)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    command.BuildingUnitPersistentLocalId,
+                    BuildingUnitFunction.Unknown)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    Fixture.Create<BuildingUnitPersistentLocalId>(),
+                    BuildingUnitFunction.Unknown)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Planned,
+                    commonBuildingUnitPersistentLocalId,
+                    BuildingUnitFunction.Common)
+                .Build();
+
+            Assert(new Scenario()
+                .Given(
+                    new BuildingStreamId(Fixture.Create<BuildingPersistentLocalId>()),
+                    buildingWasMigrated)
+                .When(command)
+                .Then(
+                    new Fact(new BuildingStreamId(command.BuildingPersistentLocalId), new BuildingUnitWasRetiredV2(command.BuildingPersistentLocalId, command.BuildingUnitPersistentLocalId)),
+                    new Fact(new BuildingStreamId(command.BuildingPersistentLocalId), new BuildingUnitWasNotRealizedV2(command.BuildingPersistentLocalId, commonBuildingUnitPersistentLocalId))
+                ));
+        }
+    }
+}
