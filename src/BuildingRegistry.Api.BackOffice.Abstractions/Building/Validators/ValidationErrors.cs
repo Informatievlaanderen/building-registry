f2fe9e1a3fa3649d816f3015c9dce1de3d2c52ae
Arne Dumarey <dumarey.arne@gmail.com>
2022-11-08 17:28:26 +0100

    fix: error message missing punctuation mark

--- src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrors.cs
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Building/Validators/ValidationErrors.cs
@@ -49,7 +49,7 @@ namespace BuildingRegistry.Api.BackOffice.Abstractions.Building.Validators
             public const string BuildingNotFound = "Onbestaand gebouw.";
             public const string BuildingRemoved = "Verwijderd gebouw.";
             public const string InvalidPolygonGeometry = "Ongeldig formaat geometriePolygoon.";
-            public const string BuildingOutlineCannotBeChanged = "Deze actie is enkel toegestaan op gebouwen met status 'gepland', 'inAanbouw' of 'gerealiseerd'";
+            public const string BuildingOutlineCannotBeChanged = "Deze actie is enkel toegestaan op gebouwen met status 'gepland', 'inAanbouw' of 'gerealiseerd'.";
             public const string BuildingCannotBePlacedUnderConstruction = "Deze actie is enkel toegestaan op gebouwen met status 'gepland'.";
             public const string BuildingCannotCorrectPlacedUnderConstruction = "Deze actie is enkel toegestaan op gebouwen met status 'inAanbouw'.";
             public const string BuildingCannotBeRealizedException = "Deze actie is enkel toegestaan op gebouwen met status 'inAanbouw'.";
