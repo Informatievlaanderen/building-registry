9049e239945e570dade56ca8c34d56a40282e39b
emalfroy <egon.malfroy@vlaanderen.be>
2022-11-14 15:24:19 +0100

    feat: add regularize bu backoffice

---
+++ src/BuildingRegistry.Api.BackOffice.Handlers.Lambda/Handlers/BuildingUnit/RegularizeBuildingUnitLambdaHandler.cs
@@ -0,0 +1,74 @@
+namespace BuildingRegistry.Api.BackOffice.Handlers.Lambda.Handlers.BuildingUnit
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Abstractions.Validation;
+    using Be.Vlaanderen.Basisregisters.AggregateSource;
+    using Be.Vlaanderen.Basisregisters.Sqs.Exceptions;
+    using Be.Vlaanderen.Basisregisters.Sqs.Lambda.Handlers;
+    using Be.Vlaanderen.Basisregisters.Sqs.Lambda.Infrastructure;
+    using Be.Vlaanderen.Basisregisters.Sqs.Responses;
+    using BuildingRegistry.Api.BackOffice.Abstractions.Building.Validators;
+    using BuildingRegistry.Building;
+    using BuildingRegistry.Building.Exceptions;
+    using Microsoft.Extensions.Configuration;
+    using Requests.BuildingUnit;
+    using TicketingService.Abstractions;
+
+    public sealed class RegularizeBuildingUnitLambdaHandler : BuildingUnitLambdaHandler<RegularizeBuildingUnitLambdaRequest>
+    {
+        public RegularizeBuildingUnitLambdaHandler(
+            IConfiguration configuration,
+            ICustomRetryPolicy retryPolicy,
+            ITicketing ticketing,
+            IIdempotentCommandHandler idempotentCommandHandler,
+            IBuildings buildings)
+            : base(
+                configuration,
+                retryPolicy,
+                ticketing,
+                idempotentCommandHandler,
+                buildings)
+        { }
+
+        protected override async Task<ETagResponse> InnerHandle(RegularizeBuildingUnitLambdaRequest request, CancellationToken cancellationToken)
+        {
+            var cmd = request.ToCommand();
+
+            try
+            {
+                await IdempotentCommandHandler.Dispatch(
+                    cmd.CreateCommandId(),
+                    cmd,
+                    request.Metadata,
+                    cancellationToken);
+            }
+            catch (IdempotencyException)
+            {
+                // Idempotent: Do Nothing return last etag
+            }
+
+            var lastHash = await GetHash(
+                request.BuildingPersistentLocalId,
+                new BuildingUnitPersistentLocalId(request.BuildingUnitPersistentLocalId),
+                cancellationToken);
+
+            return new ETagResponse(string.Format(DetailUrlFormat, request.BuildingUnitPersistentLocalId), lastHash);
+        }
+
+        protected override TicketError? InnerMapDomainException(DomainException exception, RegularizeBuildingUnitLambdaRequest request)
+        {
+            return exception switch
+            {
+                BuildingUnitHasInvalidFunctionException => new TicketError(
+                    ValidationErrorMessages.BuildingUnit.BuildingUnitHasInvalidFunction,
+                    ValidationErrorCodes.BuildingUnit.BuildingUnitHasInvalidFunction),
+                BuildingHasInvalidStatusException =>
+                    ValidationErrors.RegularizeBuildingUnit.BuildingInvalidStatus.ToTicketError(),
+                BuildingUnitHasInvalidStatusException =>
+                    ValidationErrors.RegularizeBuildingUnit.BuildingUnitInvalidStatus.ToTicketError(),
+                _ => null
+            };
+        }
+    }
+}
---
+++ test/BuildingRegistry.Tests/BackOffice/Api/WhenRegularizingBuildingUnit/GivenBuildingUnitPlanned.cs
@@ -0,0 +1,274 @@
+namespace BuildingRegistry.Tests.BackOffice.Api.WhenRegularizingBuildingUnit
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoFixture;
+    using Be.Vlaanderen.Basisregisters.AggregateSource.Snapshotting;
+    using Be.Vlaanderen.Basisregisters.Api.ETag;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.Sqs.Responses;
+    using BuildingRegistry.Api.BackOffice.Abstractions.BuildingUnit.Requests;
+    using BuildingRegistry.Api.BackOffice.Abstractions.BuildingUnit.Validators;
+    using BuildingRegistry.Api.BackOffice.Building;
+    using BuildingRegistry.Api.BackOffice.BuildingUnit;
+    using Building;
+    using Building.Events;
+    using Building.Exceptions;
+    using FluentAssertions;
+    using FluentValidation;
+    using Microsoft.AspNetCore.Http;
+    using Moq;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class GivenBuildingUnitPlanned : BackOfficeApiTest
+    {
+        private readonly BuildingUnitController _controller;
+
+        public GivenBuildingUnitPlanned(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _controller = CreateBuildingUnitControllerWithUser<BuildingUnitController>();
+        }
+
+        [Fact]
+        public async Task ThenAcceptedResponseIsExpected()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Returns(new ETagResponse(string.Empty, string.Empty));
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            var result = (AcceptedWithETagResult)await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            //Assert
+            MockMediator.Verify(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None), Times.Once);
+
+            result.StatusCode.Should().Be(202);
+            result.Location.Should().Be(string.Format(BuildingUnitDetailUrl, buildingUnitPersistentLocalId));
+        }
+
+        [Fact]
+        public async Task WithInvalidIfMatchHeader_ThenPreconditionFailedResponse()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            Fixture.Register(() => buildingUnitPersistentLocalId);
+
+            var building = new BuildingFactory(NoSnapshotStrategy.Instance, Mock.Of<IAddCommonBuildingUnit>()).Create();
+            var buildingUnitPlanned = Fixture.Create<BuildingUnitWasPlannedV2>();
+            var ifMatchHeader = "IncorrectIfMatchHeader";
+
+            building.Initialize(new[]
+            {
+                buildingUnitPlanned
+            });
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Returns(new ETagResponse(string.Empty, string.Empty));
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+
+            var result = (PreconditionFailedResult)await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(false),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                new ETag(ETagType.Strong, ifMatchHeader).ToString(),
+                CancellationToken.None);
+
+            //Assert
+            MockMediator.Verify(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None), Times.Never);
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void WhenBuildingUnitNotFound_ThenValidationException()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Throws(new BuildingUnitIsNotFoundException());
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            Func<Task> act = async () => await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            // Assert
+            act
+                .Should()
+                .ThrowAsync<ApiException>()
+                .Result
+                .Where(x =>
+                    x.Message == "Onbestaande gebouweenheid."
+                    && x.StatusCode == StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public void WhenBuildingUnitIsRemoved_ThenValidationException()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Throws(new BuildingUnitIsRemovedException(buildingUnitPersistentLocalId));
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            Func<Task> act = async () => await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            // Assert
+            act
+                .Should()
+                .ThrowAsync<ApiException>()
+                .Result
+                .Where(x =>
+                    x.Message == "Verwijderde gebouweenheid."
+                                   && x.StatusCode == StatusCodes.Status410Gone);
+        }
+
+        [Fact]
+        public void WhenBuildingUnitStatusInvalid_ThenValidationException()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Throws(new BuildingUnitHasInvalidStatusException());
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            Func<Task> act = async () => await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            // Assert
+            act
+                .Should()
+                .ThrowAsync<ValidationException>()
+                .Result
+                .Where(x =>
+                    x.Errors.Any(error
+                        => error.ErrorCode == "GebouweenheidNietGerealiseerdOfGehistoreerd"
+                                          && error.ErrorMessage == "Deze actie is enkel toegestaan op gebouweenheden met status 'gepland' of 'gerealiseerd'."));
+        }
+
+        [Fact]
+        public void WhenBuildingStatusInvalid_ThenThrowsValidationException()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Throws(new BuildingHasInvalidStatusException());
+
+            var request = new RegularizeBuildingUnitRequest
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            Func<Task> act = async () => await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            // Assert
+            act
+                .Should()
+                .ThrowAsync<ValidationException>()
+                .Result
+                .Where(x =>
+                    x.Errors.Any(error
+                        => error.ErrorCode == "GebouwStatusNietInGeplandInAanbouwOfGerealiseerd"
+                           && error.ErrorMessage == "Deze actie is enkel toegestaan binnen een gepland, inAanbouw of gerealiseerd gebouw."));
+        }
+
+        [Fact]
+        public void WhenBuildingUnitHasInvalidFunction_ThenThrowsBuildingUnitHasInvalidFunctionException()
+        {
+            var buildingUnitPersistentLocalId = new BuildingUnitPersistentLocalId(456);
+
+            MockMediator
+                .Setup(x => x.Send(It.IsAny<RegularizeBuildingUnitRequest>(), CancellationToken.None).Result)
+                .Throws(new BuildingUnitHasInvalidFunctionException());
+
+            var request = new RegularizeBuildingUnitRequest()
+            {
+                BuildingUnitPersistentLocalId = buildingUnitPersistentLocalId
+            };
+
+            //Act
+            Func<Task> act = async () => await _controller.Regularize(
+                ResponseOptions,
+                MockIfMatchValidator(true),
+                new RegularizeBuildingUnitRequestValidator(),
+                request,
+                string.Empty,
+                CancellationToken.None);
+
+            // Assert
+            act
+                .Should()
+                .ThrowAsync<ValidationException>()
+                .Result
+                .Where(x =>
+                    x.Errors.Any(error
+                        => error.ErrorCode == "GebouweenheidGemeenschappelijkDeel"
+                           && error.ErrorMessage == "Deze actie is niet toegestaan op gebouweenheden met functie gemeenschappelijkDeel."));
+        }
+    }
+}
