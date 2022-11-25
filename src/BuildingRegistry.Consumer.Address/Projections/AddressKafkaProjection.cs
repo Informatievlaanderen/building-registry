13bc8b31528ddd6d986b707c5aa69a1aaadb1b2d
pgallik <patrickgallik@gmail.com>
2022-11-25 14:11:07 +0100

    fix: how we catch dbUpdateException in address consumer

--- src/BuildingRegistry.Consumer.Address/Projections/AddressKafkaProjection.cs
+++ src/BuildingRegistry.Consumer.Address/Projections/AddressKafkaProjection.cs
@@ -1,6 +1,8 @@
 namespace BuildingRegistry.Consumer.Address.Projections
 {
     using System;
+    using System.Threading;
+    using System.Threading.Tasks;
     using Address;
     using Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.Connector;
@@ -9,11 +11,36 @@ namespace BuildingRegistry.Consumer.Address.Projections
 
     public class AddressKafkaProjection : ConnectedProjection<ConsumerAddressContext>
     {
+        private async Task CatchDbUpdateException(Func<Task> func)
+        {
+            try
+            {
+                await func();
+            }
+            catch (DbUpdateException ex)
+            {
+                const int uniqueConstraintExceptionCode = 2627;
+                const int uniqueIndexExceptionCode = 2601;
+
+                if (ex.InnerException is SqlException innerException
+                    && innerException.Number is uniqueConstraintExceptionCode or uniqueIndexExceptionCode)
+                {
+                    // When the service crashes between EF ctx saveChanges and Kafka's commit offset
+                    // it will try to reconsume the same message that was already saved to db causing duplicate key exception.
+                    // In that case ignore.
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
         public AddressKafkaProjection()
         {
             When<AddressWasMigratedToStreetName>(async (context, message, ct) =>
             {
-                try
+                await CatchDbUpdateException(async () =>
                 {
                     await context
                         .AddressConsumerItems
@@ -23,24 +50,8 @@ namespace BuildingRegistry.Consumer.Address.Projections
                                 AddressStatus.Parse(message.Status),
                                 message.IsRemoved)
                             , ct);
-                }
-                catch (DbUpdateException ex)
-                {
-                    const int uniqueConstraintExceptionCode = 2627;
-                    const int uniqueIndexExceptionCode = 2601;
-
-                    if (ex.InnerException?.InnerException is SqlException innerException
-                        && (innerException.Number == uniqueConstraintExceptionCode || innerException.Number == uniqueIndexExceptionCode))
-                    {
-                        // When the service crashes between EF ctx saveChanges and Kafka's commit offset
-                        // it will try to reconsume the same message that was already saved to db causing duplicate key exception.
-                        // In that case ignore.
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                    await context.SaveChangesAsync(CancellationToken.None);
+                });
             });
 
             When<AddressWasProposedV2>(async (context, message, ct) =>
