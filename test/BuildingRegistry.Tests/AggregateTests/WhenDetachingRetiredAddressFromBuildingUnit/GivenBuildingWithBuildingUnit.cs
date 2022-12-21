29b45174abe56900fc710fc375c8cf7c470a2dc7
jvandaal <jonas.vandaal@outlook.be>
2022-12-21 14:06:31 +0100

    add aggregate tests

---
+++ test/BuildingRegistry.Tests/AggregateTests/WhenDetachingRetiredAddressFromBuildingUnit/GivenBuildingWithBuildingUnit.cs
@@ -0,0 +1,100 @@
+namespace BuildingRegistry.Tests.AggregateTests.WhenDetachingRetiredAddressFromBuildingUnit
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoFixture;
+    using Be.Vlaanderen.Basisregisters.AggregateSource;
+    using Be.Vlaanderen.Basisregisters.AggregateSource.Snapshotting;
+    using Be.Vlaanderen.Basisregisters.AggregateSource.Testing;
+    using Building;
+    using Building.Commands;
+    using Building.Events;
+    using Extensions;
+    using FluentAssertions;
+    using Moq;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class GivenBuildingWithBuildingUnit : BuildingRegistryTest
+    {
+        public GivenBuildingWithBuildingUnit(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        { }
+
+        [Fact]
+        public void ThenApplyDetachAddressFromBuildingUnitBecauseAddressWasRetired()
+        {
+            var command = Fixture.Create<DetachAddressFromBuildingUnitBecauseAddressWasRetired>();
+
+            var buildingWasMigrated = new BuildingWasMigratedBuilder(Fixture)
+                .WithBuildingPersistentLocalId(command.BuildingPersistentLocalId)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    command.BuildingUnitPersistentLocalId,
+                    attachedAddress: new List<AddressPersistentLocalId> { command.AddressPersistentLocalId },
+                    isRemoved: false)
+                .Build();
+
+            Assert(new Scenario()
+                .Given(
+                    new BuildingStreamId(command.BuildingPersistentLocalId),
+                    buildingWasMigrated)
+                .When(command)
+                .Then(new Fact(
+                    new BuildingStreamId(command.BuildingPersistentLocalId),
+                    new BuildingUnitAddressWasDetachedBecauseAddressWasRetired(
+                        command.BuildingPersistentLocalId,
+                        command.BuildingUnitPersistentLocalId,
+                        command.AddressPersistentLocalId))));
+        }
+
+        [Fact]
+        public void WithAddressNoLongerAttached_ThenNothing()
+        {
+            var command = Fixture.Create<DetachAddressFromBuildingUnitBecauseAddressWasRetired>();
+
+            var buildingWasMigrated = new BuildingWasMigratedBuilder(Fixture)
+                .WithBuildingPersistentLocalId(command.BuildingPersistentLocalId)
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    command.BuildingUnitPersistentLocalId,
+                    attachedAddress: new List<AddressPersistentLocalId>(0),
+                    isRemoved: false)
+                .Build();
+
+            Assert(new Scenario()
+                .Given(
+                    new BuildingStreamId(command.BuildingPersistentLocalId),
+                    buildingWasMigrated)
+                .When(command)
+                .ThenNone());
+        }
+
+        [Fact]
+        public void StateCheck()
+        {
+            var buildingUnitAddressWasDetachedBecauseAddressWasRetired = Fixture.Create<BuildingUnitAddressWasDetachedBecauseAddressWasRetired>();
+            var buildingWasMigrated = new BuildingWasMigratedBuilder(Fixture)
+                .WithBuildingPersistentLocalId(new BuildingPersistentLocalId(buildingUnitAddressWasDetachedBecauseAddressWasRetired.BuildingPersistentLocalId))
+                .WithBuildingUnit(
+                    BuildingRegistry.Legacy.BuildingUnitStatus.Realized,
+                    new BuildingUnitPersistentLocalId(buildingUnitAddressWasDetachedBecauseAddressWasRetired.BuildingUnitPersistentLocalId),
+                    attachedAddress: new List<AddressPersistentLocalId>
+                    {
+                        new AddressPersistentLocalId(buildingUnitAddressWasDetachedBecauseAddressWasRetired.AddressPersistentLocalId),
+                        new AddressPersistentLocalId(123),
+                    },
+                    isRemoved: false)
+                .Build();
+
+            var building = new BuildingFactory(NoSnapshotStrategy.Instance, Mock.Of<IAddCommonBuildingUnit>(), Mock.Of<IAddresses>()).Create();
+            building.Initialize(new List<object>
+            {
+                buildingWasMigrated,
+                buildingUnitAddressWasDetachedBecauseAddressWasRetired
+            });
+
+            building.BuildingUnits.First().AddressPersistentLocalIds.Should().BeEquivalentTo(
+                new List<AddressPersistentLocalId>{ new AddressPersistentLocalId(123) });
+        }
+    }
+}
