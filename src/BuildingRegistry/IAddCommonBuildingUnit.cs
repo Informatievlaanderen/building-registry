f8c0ee1d85338ea5c8560aa8a0fbfa4576eee9e9
jvandaal <jonas.vandaal@outlook.be>
2023-01-03 14:56:15 +0100

    fix: trigger

--- src/BuildingRegistry/IAddCommonBuildingUnit.cs
+++ src/BuildingRegistry/IAddCommonBuildingUnit.cs
@@ -4,8 +4,6 @@
 
     public interface IAddCommonBuildingUnit : IPersistentLocalIdGenerator
     {
-        void AddForBuilding(
-            BuildingPersistentLocalId buildingPersistentLocalId,
-            BuildingUnitPersistentLocalId buildingUnitPersistentLocalId);
+        void AddForBuilding(BuildingPersistentLocalId buildingPersistentLocalId, BuildingUnitPersistentLocalId buildingUnitPersistentLocalId);
     }
 }
