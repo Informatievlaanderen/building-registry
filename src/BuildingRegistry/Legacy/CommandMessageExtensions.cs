0317aa15903cd84034eb5684366cc1021255d3ed
Luk Vermeulen <luk.vermeulen@gmail.com>
2023-01-26 10:33:48 +0100

    fix: use return value of Metadata.Add

--- src/BuildingRegistry/Legacy/CommandMessageExtensions.cs
+++ src/BuildingRegistry/Legacy/CommandMessageExtensions.cs
@@ -8,16 +8,23 @@ namespace BuildingRegistry.Legacy
     {
         public static void AddMetadata(this CommandMessage commandMessage, string key, object value)
         {
-            if (commandMessage.Metadata.ContainsKey(key))
-                throw new ArgumentException($"Element with key '{key}' already exists.");
+            if (commandMessage.Metadata is not null)
+            {
+                if (commandMessage.Metadata.ContainsKey(key))
+                {
+                    throw new ArgumentException($"Element with key '{key}' already exists.");
+                }
 
-            commandMessage.Metadata.Add(key, value);
+                _ = commandMessage.Metadata.Add(key, value);
+            }
         }
 
         public static void AddMetadata(this CommandMessage commandMessage, IDictionary<string, object> metadataDictionary)
         {
             foreach (var keyValuePair in metadataDictionary)
+            {
                 AddMetadata(commandMessage, keyValuePair.Key, keyValuePair.Value);
+            }
         }
     }
 }
