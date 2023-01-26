6fb5cdd153d421960df7aff95a8bd09620e71ad1
Arne Dumarey <dumarey.arne@gmail.com>
2023-01-26 11:44:12 +0100

    Fix docker file backoffice projections (#894)
    * fix: remove dockerfile & init.sh
    
    * chore: readd dockerfile & init.sh

--- src/BuildingRegistry.Projections.BackOffice/init.sh
+++ src/BuildingRegistry.Projections.BackOffice/init.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/sh
+#!/bin/sh
 
 CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | startswith(\"basisregisters-\") and endswith(\"-projections-backoffice\")) | .DockerId")
 
