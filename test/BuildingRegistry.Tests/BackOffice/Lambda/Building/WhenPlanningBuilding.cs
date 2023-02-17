3625b4557b1ac6aec61bb1ba2e231ce8c343e760
emalfroy <egon.malfroy@gmail.com>
2023-02-17 11:48:39 +0100

    add try catch around idempotency lambda handler and add snapshot registration backoffice

--- test/BuildingRegistry.Tests/BackOffice/Lambda/Building/WhenPlanningBuilding.cs
+++ test/BuildingRegistry.Tests/BackOffice/Lambda/Building/WhenPlanningBuilding.cs
@@ -10,6 +10,7 @@ namespace BuildingRegistry.Tests.BackOffice.Lambda.Building
     using Be.Vlaanderen.Basisregisters.CommandHandling;
     using Be.Vlaanderen.Basisregisters.CommandHandling.Idempotency;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using Be.Vlaanderen.Basisregisters.Sqs.Exceptions;
     using Be.Vlaanderen.Basisregisters.Sqs.Lambda.Handlers;
     using Be.Vlaanderen.Basisregisters.Sqs.Responses;
     using BuildingRegistry.Api.BackOffice.Abstractions.Building.Requests;
@@ -17,12 +18,14 @@ namespace BuildingRegistry.Tests.BackOffice.Lambda.Building
     using BuildingRegistry.Api.BackOffice.Handlers.Lambda.Handlers.Building;
     using BuildingRegistry.Api.BackOffice.Handlers.Lambda.Requests.Building;
     using BuildingRegistry.Building;
+    using BuildingRegistry.Infrastructure.Repositories;
     using Fixtures;
     using FluentAssertions;
     using Microsoft.Extensions.Configuration;
     using Moq;
     using SqlStreamStore;
     using SqlStreamStore.Streams;
+    using TicketingService.Abstractions;
     using Xunit;
     using Xunit.Abstractions;
 
@@ -42,10 +45,6 @@ namespace BuildingRegistry.Tests.BackOffice.Lambda.Building
         {
             // Arrange
             var expectedBuildingPersistentLocalId = Fixture.Create<BuildingPersistentLocalId>();
-            var persistentLocalIdGenerator = new Mock<IPersistentLocalIdGenerator>();
-            persistentLocalIdGenerator
-                .Setup(x => x.GenerateNextPersistentLocalId())
-                .Returns(expectedBuildingPersistentLocalId);
 
             var eTagResponse = new ETagResponse(string.Empty, Fixture.Create<string>());
             var handler = new PlanBuildingLambdaHandler(
@@ -79,6 +78,107 @@ namespace BuildingRegistry.Tests.BackOffice.Lambda.Building
             var message = stream.Messages.First();
             message.JsonMetadata.Should().Contain(eTagResponse.ETag);
             message.JsonMetadata.Should().Contain(Provenance.ProvenanceMetadataKey.ToLower());
+        }
+
+        [Fact]
+        public async Task WhenIdempotencyException_ThenTicketingCompleteIsExpected()
+        {
+            // Arrange
+            var ticketing = new Mock<ITicketing>();
+            var buildingPersistentLocalId = Fixture.Create<BuildingPersistentLocalId>();
+            var buildings = Container.Resolve<IBuildings>();
+
+            var handler = new PlanBuildingLambdaHandler(
+                Container.Resolve<IConfiguration>(),
+                new FakeRetryPolicy(),
+                ticketing.Object,
+                MockExceptionIdempotentCommandHandler(() => new IdempotencyException(string.Empty)).Object,
+                buildings);
+
+            PlanBuilding(buildingPersistentLocalId);
+
+            var building =
+                await buildings.GetAsync(new BuildingStreamId(buildingPersistentLocalId), CancellationToken.None);
+
+            //Act
+            await handler.Handle(
+                new PlanBuildingLambdaRequest(
+                    buildingPersistentLocalId,
+                    new PlanBuildingSqsRequest()
+                    {
+                        BuildingPersistentLocalId = buildingPersistentLocalId,
+                        IfMatchHeaderValue = null,
+                        Metadata = new Dictionary<string, object?>(),
+                        ProvenanceData = Fixture.Create<ProvenanceData>(),
+                        TicketId = Guid.NewGuid(),
+                        Request = new PlanBuildingRequest
+                        {
+                            GeometriePolygoon =
+                                "<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>"
+                        }
+                    }),
+                CancellationToken.None);
+
+            
+            //Assert
+            ticketing.Verify(x =>
+                x.Complete(
+                    It.IsAny<Guid>(),
+                    new TicketResult(
+                        new ETagResponse(
+                            string.Format(ConfigDetailUrl, buildingPersistentLocalId),
+                            building.LastEventHash)),
+                    CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task GivenRetryingRequest_ThenBuildingIsPlanned()
+        {
+            // Arrange
+            var ticketing = new Mock<ITicketing>();
+            var buildingPersistentLocalId = Fixture.Create<BuildingPersistentLocalId>();
+
+            var buildings = Container.Resolve<IBuildings>();
+            var eTagResponse = new ETagResponse(string.Empty, Fixture.Create<string>());
+            var handler = new PlanBuildingLambdaHandler(
+                Container.Resolve<IConfiguration>(),
+                new FakeRetryPolicy(),
+                ticketing.Object,
+                new IdempotentCommandHandler(Container.Resolve<ICommandHandlerResolver>(), _idempotencyContext),
+                buildings);
+
+            var request = new PlanBuildingLambdaRequest(
+                buildingPersistentLocalId,
+                new PlanBuildingSqsRequest()
+                {
+                    BuildingPersistentLocalId = buildingPersistentLocalId,
+                    IfMatchHeaderValue = null,
+                    Metadata = new Dictionary<string, object?>(),
+                    ProvenanceData = Fixture.Create<ProvenanceData>(),
+                    TicketId = Guid.NewGuid(),
+                    Request = new PlanBuildingRequest
+                    {
+                        GeometriePolygoon =
+                            "<gml:Polygon srsName=\"https://www.opengis.net/def/crs/EPSG/0/31370\" xmlns:gml=\"http://www.opengis.net/gml/3.2\"><gml:exterior><gml:LinearRing><gml:posList>140284.15277253836 186724.74131567031 140291.06016454101 186726.38355567306 140288.22675654292 186738.25798767805 140281.19098053873 186736.57913967967 140284.15277253836 186724.74131567031</gml:posList></gml:LinearRing></gml:exterior></gml:Polygon>"
+                    }
+                });
+
+            //Act
+            await handler.Handle(request, CancellationToken.None);
+            await handler.Handle(request, CancellationToken.None);
+
+            var building =
+                await buildings.GetAsync(new BuildingStreamId(buildingPersistentLocalId), CancellationToken.None);
+
+            //Assert
+            ticketing.Verify(x =>
+                x.Complete(
+                    It.IsAny<Guid>(),
+                    new TicketResult(
+                        new ETagResponse(
+                            string.Format(ConfigDetailUrl, buildingPersistentLocalId),
+                            building.LastEventHash)),
+                    CancellationToken.None));
         }
     }
 }
