92f72008346420a1975d564baa57137b180ea29f
emalfroy <egon.malfroy@gmail.com>
2023-03-17 13:40:06 +0100

    feat: add authorization for grb upload

--- src/BuildingRegistry.Api.Grb/Uploads/UploadController.cs
+++ src/BuildingRegistry.Api.Grb/Uploads/UploadController.cs
@@ -1,9 +1,12 @@
-﻿namespace BuildingRegistry.Api.Grb.Uploads
+namespace BuildingRegistry.Api.Grb.Uploads
 {
     using System.Threading;
     using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.AcmIdm;
     using Be.Vlaanderen.Basisregisters.Api;
     using MediatR;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
     [ApiVersion("2.0")]
@@ -20,6 +23,7 @@
         }
 
         [HttpPost("job")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = PolicyNames.IngemetenGebouw.GrbBijwerker)]
         public async Task<IActionResult> GetPreSignedUrl(CancellationToken cancellationToken)
         {
             return Ok(await _mediator.Send(new GetPreSignedUrlRequest(), cancellationToken));
