64d98b30854a63c47dd561620acb9f8babaef7c5
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-27 19:32:08 +0200

    test: fix flaky tests

---
+++ test/BuildingRegistry.Tests/Fixtures/RandomUniqueIntegerGenerator.cs
@@ -0,0 +1,29 @@
+﻿namespace BuildingRegistry.Tests.Fixtures
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoFixture.Kernel;
+
+    public class RandomUniqueIntegerGenerator : ISpecimenBuilder
+    {
+        private readonly Random _random = new Random();
+
+        private readonly HashSet<int> _generatedIntegers = new HashSet<int>();
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (request is Type type && type == typeof(int))
+            {
+                int nextInt;
+                do
+                {
+                    nextInt = _random.Next();
+                } while (_generatedIntegers.Contains(nextInt));
+                _generatedIntegers.Add(nextInt);
+                return nextInt;
+            }
+
+            return new NoSpecimen();
+        }
+    }
+}
