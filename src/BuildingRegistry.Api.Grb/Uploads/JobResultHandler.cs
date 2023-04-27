c7e123126d5171a5ed97b31f094d0973c56e188d
Arne Dumarey <dumarey.arne@gmail.com>
2023-04-27 21:34:17 +0200

    feat: grb api add GetResult

---
+++ src/BuildingRegistry.Api.Grb/Uploads/JobResultHandler.cs
@@ -0,0 +1,67 @@
+﻿namespace BuildingRegistry.Api.Grb.Uploads
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Api.Exceptions;
+    using Be.Vlaanderen.Basisregisters.Api.Extract;
+    using Be.Vlaanderen.Basisregisters.GrAr.Extracts;
+    using BuildingRegistry.Grb.Abstractions;
+    using MediatR;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+
+    public sealed record GetJobResultRequest(Guid JobId) : IRequest<ExtractArchive>;
+
+    public sealed class JobResultHandler : IRequestHandler<GetJobResultRequest, ExtractArchive>
+    {
+        private readonly BuildingGrbContext _buildingGrbContext;
+
+        public JobResultHandler(BuildingGrbContext buildingGrbContext)
+        {
+            _buildingGrbContext = buildingGrbContext;
+        }
+
+        public async Task<ExtractArchive> Handle(GetJobResultRequest request, CancellationToken cancellationToken)
+        {
+            var job = await _buildingGrbContext.Jobs.FindAsync(new object[] { request.JobId }, cancellationToken);
+
+            if (job is not { Status: JobStatus.Completed })
+            {
+                throw new ApiException($"Upload job with id {request.JobId} not found or not completed.", StatusCodes.Status404NotFound);
+            }
+
+            return new ExtractArchive($"JobResults_{job.Id:D}.zip")
+                {
+                    CreateResultFile(job.Id)
+                };
+        }
+
+        private ExtractFile CreateResultFile(Guid jobId)
+        {
+            var resultItems = _buildingGrbContext
+                .JobResults
+                .AsNoTracking()
+                .Where(result => result.JobId == jobId && result.IsBuildingCreated);
+
+            byte[] TransformRecord(JobResult jobResult)
+            {
+                var item = new JobResultDbaseRecord
+                {
+                    idn = { Value = jobResult.GrbIdn },
+                    grid = { Value = jobResult.BuildingPersistentLocalId },
+                };
+
+                return item.ToBytes(DbfFileWriter<JobResultDbaseRecord>.Encoding);
+            }
+
+            return ExtractBuilder.CreateDbfFile<JobResult, JobResultDbaseRecord>(
+                "IdnGrResults",
+                new JobResultDbaseSchema(),
+                resultItems,
+                resultItems.Count,
+                TransformRecord);
+        }
+    }
+}
--- test/BuildingRegistry.Api.Grb.IntegrationTests/AuthorizationIntegrationTests.cs
+++ test/BuildingRegistry.Api.Grb.IntegrationTests/AuthorizationIntegrationTests.cs
@@ -20,7 +20,7 @@ using Be.Vlaanderen.Basisregisters.Auth.AcmIdm;
 
         [Theory]
         [InlineData("/v2/uploads/job", Scopes.DvGrIngemetengebouwBeheer)]
-        public async Task ReturnsSuccess(string endpoint, string requiredScopes)
+        public async Task Post_ReturnsSuccess(string endpoint, string requiredScopes)
         {
             var client = _fixture.TestServer.CreateClient();
             client.DefaultRequestHeaders.Authorization =
@@ -35,7 +35,7 @@ using Be.Vlaanderen.Basisregisters.Auth.AcmIdm;
 
         [Theory]
         [InlineData("/v2/uploads/job")]
-        public async Task ReturnsUnauthorized(string endpoint)
+        public async Task Post_ReturnsUnauthorized(string endpoint)
         {
             var client = _fixture.TestServer.CreateClient();
 
@@ -48,7 +48,7 @@ using Be.Vlaanderen.Basisregisters.Auth.AcmIdm;
         [Theory]
         [InlineData("/v2/uploads/job")]
         [InlineData("/v2/uploads/job", "dv_gr_geschetstgebouw_beheer")]
-        public async Task ReturnsForbidden(string endpoint, string scope = "")
+        public async Task Post_ReturnsForbidden(string endpoint, string scope = "")
         {
             var client = _fixture.TestServer.CreateClient();
             client.DefaultRequestHeaders.Authorization =
@@ -56,6 +56,45 @@ using Be.Vlaanderen.Basisregisters.Auth.AcmIdm;
 
             var response = await client.PostAsync(endpoint,
                 new StringContent("{}", Encoding.UTF8, "application/json"), CancellationToken.None);
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/v2/uploads/jobs/00000000-0000-0000-0000-000000000000", Scopes.DvGrIngemetengebouwBeheer)]
+        public async Task Get_ReturnsSuccess(string endpoint, string requiredScopes)
+        {
+            var client = _fixture.TestServer.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", await _fixture.GetAccessToken(requiredScopes));
+
+            var response = await client.GetAsync(endpoint, CancellationToken.None);
+            Assert.NotNull(response);
+            Assert.NotEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+            Assert.NotEqual(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/v2/uploads/jobs/00000000-0000-0000-0000-000000000000")]
+        public async Task Get_ReturnsUnauthorized(string endpoint)
+        {
+            var client = _fixture.TestServer.CreateClient();
+
+            var response = await client.GetAsync(endpoint, CancellationToken.None);
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/v2/uploads/jobs/00000000-0000-0000-0000-000000000000")]
+        [InlineData("/v2/uploads/jobs/00000000-0000-0000-0000-000000000000", "dv_gr_geschetstgebouw_beheer")]
+        public async Task Get_ReturnsForbidden(string endpoint, string scope = "")
+        {
+            var client = _fixture.TestServer.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", await _fixture.GetAccessToken(scope));
+
+            var response = await client.GetAsync(endpoint, CancellationToken.None);
             Assert.NotNull(response);
             Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
         }
