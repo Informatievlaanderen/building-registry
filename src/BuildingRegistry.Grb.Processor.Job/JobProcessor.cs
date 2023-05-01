c7d35bfce9927e720de1a5d460fec319e933cfba
Arne Dumarey <dumarey.arne@gmail.com>
2023-05-01 12:47:46 +0200

    chore: add skeleton for upload/job processor

---
+++ src/BuildingRegistry.Grb.Processor.Job/JobProcessor.cs
@@ -0,0 +1,19 @@
+﻿namespace BuildingRegistry.Grb.Processor.Job
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+
+    public sealed class JobProcessor : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Monitor Job, start in sequence
+            // Process Job Records
+            // Process Job Records tickets
+            // Create result set
+            // Archive Job
+            throw new System.NotImplementedException();
+        }
+    }
+}
