eb4932e12ba965157ecdc595b873b82d788541b8
Arne Dumarey <dumarey.arne@gmail.com>
2023-05-01 12:47:46 +0200

    chore: add skeleton for upload/job processor

---
+++ src/BuildingRegistry.Grb.Processor.Upload/UploadProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace BuildingRegistry.Grb.Processor.Upload
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+
+    public sealed class UploadProcessor : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Check created jobs
+            // See if there's a S3 object with job id
+            // If so, update ticket status and job status => preparing
+            // extract, verify, and store data as job records
+            // update job status => prepared
+            // trigger job processor
+            throw new System.NotImplementedException();
+        }
+    }
+}
