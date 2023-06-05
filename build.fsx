b18b0153d39451882026d9c2591383bfed92b320
Arne Dumarey <dumarey.arne@gmail.com>
2023-06-05 08:29:43 +0200

    ci: remove GRB from ci/cd pipeline

--- build.fsx
+++ build.fsx
@@ -61,12 +61,9 @@ Target.create "Publish_Solution" (fun _ ->
     "BuildingRegistry.Api.Extract"
     "BuildingRegistry.Api.CrabImport"
     "BuildingRegistry.Api.BackOffice"
-    "BuildingRegistry.Api.Grb"
     "BuildingRegistry.Api.BackOffice.Abstractions"
     "BuildingRegistry.Api.BackOffice.Handlers.Lambda"
     "BuildingRegistry.Consumer.Address"
-    "BuildingRegistry.Grb.Processor.Upload"
-    "BuildingRegistry.Grb.Processor.Job"
     "BuildingRegistry.Projections.Legacy"
     "BuildingRegistry.Projections.Extract"
     "BuildingRegistry.Projections.LastChangedList"
@@ -100,9 +97,6 @@ Target.create "Containerize_ApiCrabImport" (fun _ ->
   //Shell.copyFile dist (source @@ "SqlStreamStore.MsSql.dll")
 
   containerize "BuildingRegistry.Api.CrabImport" "api-crab-import")
-Target.create "Containerize_ApiGrb" (fun _ -> containerize "BuildingRegistry.Api.Grb" "api-grb")
-Target.create "Containerize_GrbProcessorUpload" (fun _ -> containerize "BuildingRegistry.Grb.Processor.Upload" "grb-processor-upload")
-Target.create "Containerize_GrbProcessorJob" (fun _ -> containerize "BuildingRegistry.Grb.Processor.Job" "grb-processor-job")
 Target.create "Containerize_ProjectionsSyndication" (fun _ -> containerize "BuildingRegistry.Projections.Syndication" "projections-syndication")
 Target.create "Containerize_ProjectionsBackOffice" (fun _ -> containerize "BuildingRegistry.Projections.BackOffice" "projections-backoffice")
 Target.create "Containerize_ConsumerAddress" (fun _ -> containerize "BuildingRegistry.Consumer.Address" "consumer-address")
