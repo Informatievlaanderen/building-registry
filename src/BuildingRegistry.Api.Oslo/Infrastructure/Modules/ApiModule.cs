9aec63840a1968488124144cf779448cb45a41e9
jvandaal <jonas.vandaal@outlook.be>
2023-06-14 11:58:20 +0200

    fix: register consumer parcel module in legacy and oslo api

--- src/BuildingRegistry.Api.Oslo/Infrastructure/Modules/ApiModule.cs
+++ src/BuildingRegistry.Api.Oslo/Infrastructure/Modules/ApiModule.cs
@@ -5,6 +5,7 @@ namespace BuildingRegistry.Api.Oslo.Infrastructure.Modules
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Be.Vlaanderen.Basisregisters.DataDog.Tracing.Microsoft;
     using Be.Vlaanderen.Basisregisters.DependencyInjection;
+    using Consumer.Read.Parcel.Infrastructure.Modules;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
@@ -43,7 +44,8 @@ namespace BuildingRegistry.Api.Oslo.Infrastructure.Modules
                 .RegisterModule(new MediatRModule(useProjectionsV2))
                 .RegisterModule(new LegacyModule(_configuration, _services, _loggerFactory))
                 .RegisterModule(new SyndicationModule(_configuration, _services, _loggerFactory))
-                .RegisterModule(new GrbModule(_configuration));
+                .RegisterModule(new GrbModule(_configuration))
+                .RegisterModule(new ConsumerParcelModule(_configuration, _services, _loggerFactory));
 
             builder
                 .RegisterType<ProblemDetailsHelper>()
