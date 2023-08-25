6cd00eb1b6a64d9de5dcbe8107fb37fea872cac6
Arne Dumarey <dumarey.arne@gmail.com>
2023-08-25 14:05:31 +0200

    style: index naming

--- src/BuildingRegistry.Projections.Wfs/Migrations/20230825093350_AddSpatialIndexesV2.cs
+++ src/BuildingRegistry.Projections.Wfs/Migrations/20230825093350_AddSpatialIndexesV2.cs
@@ -9,7 +9,7 @@ namespace BuildingRegistry.Projections.Wfs.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-	            CREATE SPATIAL INDEX [SPATIAL_GebouwV2_Geometrie] ON [wfs].[buildingsV2] ([Geometry])
+	            CREATE SPATIAL INDEX [SPATIAL_BuildingsV2_Geometry] ON [wfs].[buildingsV2] ([Geometry])
 	            USING  GEOMETRY_GRID
 	            WITH (
 		            BOUNDING_BOX =(22279.17, 153050.23, 258873.3, 244022.31),
@@ -22,7 +22,7 @@ namespace BuildingRegistry.Projections.Wfs.Migrations
 	            GO");
 
             migrationBuilder.Sql(@"
-                CREATE SPATIAL INDEX [SPATIAL_GebouweenheidV2_Geometrie] ON [wfs].[buildingUnitsV2] ([Position])
+                CREATE SPATIAL INDEX [SPATIAL_BuildingUnitsV2_Position] ON [wfs].[buildingUnitsV2] ([Position])
                 USING  GEOMETRY_GRID
                 WITH (
                     BOUNDING_BOX =(22279.17, 153050.23, 258873.3, 244022.31),
@@ -39,11 +39,11 @@ namespace BuildingRegistry.Projections.Wfs.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                DROP INDEX [SPATIAL_GebouweenheidV2_Geometrie] ON [wfs].[buildingUnitsV2]
+                DROP INDEX [SPATIAL_BuildingUnitsV2_Position] ON [wfs].[buildingUnitsV2]
                 GO");
 
             migrationBuilder.Sql(@"
-                DROP INDEX [SPATIAL_GebouwV2_Geometrie] ON [wfs].[buildingsV2]
+                DROP INDEX [SPATIAL_BuildingsV2_Geometry] ON [wfs].[buildingsV2]
                 GO");
         }
     }
