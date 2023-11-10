bb888d8b56b36b3d6211abbb02d0c323b3258b30
emalfroy <egon.malfroy@gmail.com>
2023-11-10 10:33:16 +0100

    refactor: producer

---
+++ src/BuildingRegistry.Producer/BuildingProducer.cs
@@ -0,0 +1,40 @@
+﻿namespace BuildingRegistry.Producer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class BuildingProducer : BackgroundService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger<BuildingProducer> _logger;
+        private readonly IConnectedProjectionsManager _projectionManager;
+
+        public BuildingProducer(
+            IHostApplicationLifetime hostApplicationLifetime,
+            IConnectedProjectionsManager projectionManager,
+            ILogger<BuildingProducer> logger)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+            _projectionManager = projectionManager;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await _projectionManager.Start(stoppingToken);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogCritical(exception, $"Critical error occured in {nameof(BuildingProducer)}.");
+                _hostApplicationLifetime.StopApplication();
+                throw;
+            }
+        }
+    }
+}
--- src/BuildingRegistry.Producer/Infrastructure/Modules/ProducerModule.cs
+++ src/BuildingRegistry.Producer/Infrastructure/Modules/ProducerModule.cs
@@ -5,29 +5,31 @@ namespace BuildingRegistry.Producer.Infrastructure.Modules
     using Autofac.Extensions.DependencyInjection;
     using Be.Vlaanderen.Basisregisters.Api.Exceptions;
     using Be.Vlaanderen.Basisregisters.DataDog.Tracing.Microsoft;
+    using Be.Vlaanderen.Basisregisters.DataDog.Tracing.Sql.EntityFrameworkCore;
     using Be.Vlaanderen.Basisregisters.DependencyInjection;
     using Be.Vlaanderen.Basisregisters.EventHandling;
     using Be.Vlaanderen.Basisregisters.EventHandling.Autofac;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Producer;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner.MigrationExtensions;
     using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Autofac;
     using Be.Vlaanderen.Basisregisters.Projector;
     using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
     using Be.Vlaanderen.Basisregisters.Projector.Modules;
+    using BuildingRegistry.Infrastructure;
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
-    using BuildingRegistry.Infrastructure;
-    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka;
-    using Be.Vlaanderen.Basisregisters.MessageHandling.Kafka.Producer;
-    using Producer;
-    using BuildingRegistry;
 
-    public class ApiModule : Module
+    public class ProducerModule : Module
     {
         private readonly IConfiguration _configuration;
         private readonly IServiceCollection _services;
         private readonly ILoggerFactory _loggerFactory;
 
-        public ApiModule(
+        public ProducerModule(
             IConfiguration configuration,
             IServiceCollection services,
             ILoggerFactory loggerFactory)
@@ -66,12 +68,26 @@ namespace BuildingRegistry.Producer.Infrastructure.Modules
 
         private void RegisterProjections(ContainerBuilder builder)
         {
-            builder
-                .RegisterModule(
-                    new ProducerModule(
-                        _configuration,
-                        _services,
-                        _loggerFactory));
+            var logger = _loggerFactory.CreateLogger<ProducerModule>();
+            var connectionString = _configuration.GetConnectionString("ProducerProjections");
+
+            var hasConnectionString = !string.IsNullOrWhiteSpace(connectionString);
+            if (hasConnectionString)
+            {
+                RunOnSqlServer(_configuration, _services, _loggerFactory, connectionString);
+            }
+            else
+            {
+                RunInMemoryDb(_services, _loggerFactory, logger);
+            }
+
+            logger.LogInformation(
+                "Added {Context} to services:" +
+                Environment.NewLine +
+                "\tSchema: {Schema}" +
+                Environment.NewLine +
+                "\tTableName: {TableName}",
+                nameof(ProducerContext), Schema.Producer, MigrationTables.Producer);
 
             var connectedProjectionSettings = ConnectedProjectionSettings.Configure(x =>
             {
@@ -87,25 +103,26 @@ namespace BuildingRegistry.Producer.Infrastructure.Modules
                 .RegisterProjectionMigrator<ProducerContextMigrationFactory>(
                     _configuration,
                     _loggerFactory)
-                .RegisterProjections<ProducerProjections, ProducerContext>(() =>
-                {
-                    var topic = $"{_configuration[ProducerProjections.TopicKey]}" ?? throw new ArgumentException($"Configuration has no value for {ProducerProjections.TopicKey}");
-                    var producerOptions = new ProducerOptions(
-                        new BootstrapServers(bootstrapServers),
-                        new Topic(topic),
-                        true,
-                        EventsJsonSerializerSettingsProvider.CreateSerializerSettings())
-                        .ConfigureEnableIdempotence();
-                    if (!string.IsNullOrEmpty(saslUserName)
-                        && !string.IsNullOrEmpty(saslPassword))
-                    {
-                        producerOptions.ConfigureSaslAuthentication(new SaslAuthentication(
-                            saslUserName,
-                            saslPassword));
-                    }
-
-                    return new ProducerProjections(new Producer(producerOptions));
-                }, connectedProjectionSettings)
+                // TODO: see if we still need to implements this
+                // .RegisterProjections<ProducerProjections, ProducerContext>(() =>
+                // {
+                //     var topic = $"{_configuration[ProducerProjections.TopicKey]}" ?? throw new ArgumentException($"Configuration has no value for {ProducerProjections.TopicKey}");
+                //     var producerOptions = new ProducerOptions(
+                //         new BootstrapServers(bootstrapServers),
+                //         new Topic(topic),
+                //         true,
+                //         EventsJsonSerializerSettingsProvider.CreateSerializerSettings())
+                //         .ConfigureEnableIdempotence();
+                //     if (!string.IsNullOrEmpty(saslUserName)
+                //         && !string.IsNullOrEmpty(saslPassword))
+                //     {
+                //         producerOptions.ConfigureSaslAuthentication(new SaslAuthentication(
+                //             saslUserName,
+                //             saslPassword));
+                //     }
+                //
+                //     return new ProducerProjections(new Producer(producerOptions));
+                // }, connectedProjectionSettings)
                 .RegisterProjections<ProducerMigrateProjections, ProducerContext>(() =>
                 {
                     var topic = $"{_configuration[ProducerMigrateProjections.TopicKey]}" ?? throw new ArgumentException($"Configuration has no value for {ProducerMigrateProjections.TopicKey}");
@@ -126,6 +143,39 @@ namespace BuildingRegistry.Producer.Infrastructure.Modules
 
                     return new ProducerMigrateProjections(new Producer(producerOptions));
                 }, connectedProjectionSettings);
+        }
+
+        private static void RunOnSqlServer(
+            IConfiguration configuration,
+            IServiceCollection services,
+            ILoggerFactory loggerFactory,
+            string backofficeProjectionsConnectionString)
+        {
+            services
+                .AddScoped(_ => new TraceDbConnection<ProducerContext>(
+                    new SqlConnection(backofficeProjectionsConnectionString),
+                    configuration["DataDog:ServiceName"]))
+                .AddDbContext<ProducerContext>((provider, options) => options
+                    .UseLoggerFactory(loggerFactory)
+                    .UseSqlServer(provider.GetRequiredService<TraceDbConnection<ProducerContext>>(), sqlServerOptions =>
+                    {
+                        sqlServerOptions.EnableRetryOnFailure();
+                        sqlServerOptions.MigrationsHistoryTable(MigrationTables.Producer, Schema.Producer);
+                    })
+                    .UseExtendedSqlServerMigrations());
+        }
+
+        private static void RunInMemoryDb(
+            IServiceCollection services,
+            ILoggerFactory loggerFactory,
+            ILogger logger)
+        {
+            services
+                .AddDbContext<ProducerContext>(options => options
+                    .UseLoggerFactory(loggerFactory)
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString(), _ => { }));
+
+            logger.LogWarning("Running InMemory for {Context}!", nameof(ProducerContext));
         }
     }
 }
