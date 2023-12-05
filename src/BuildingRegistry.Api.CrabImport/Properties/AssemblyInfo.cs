bcea1f17b24ad5d9ca672ae9d725c81bf30dc378
emalfroy <egon.malfroy@gmail.com>
2023-12-05 15:55:17 +0100

    chore: remove crabimport

--- src/BuildingRegistry.Api.CrabImport/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("BuildingRegistry CrabImport API")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("62c5736a-4089-4fd2-9817-019b992dbf25")]
--- test/BuildingRegistry.Console.SortToProcess/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("BuildingRegistry Console SortToProcess")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("44e98e18-8f7d-435b-a659-47256da3bcef")]
