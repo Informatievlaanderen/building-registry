52a323f939f3912fc2166638c1eb082a48de576b
emalfroy <egon.malfroy@gmail.com>
2023-12-05 15:55:17 +0100

    chore: remove crabimport

--- test/BuildingRegistry.Console.ReactivatedHouseNumbers/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("BuildingRegistry Console Reactivated HouseNumbers")]
-
-[assembly: ComVisible(false)]
-[assembly: Guid("fccfe3bb-01be-4fec-8bf8-1bb4b9a4bbc7")]
