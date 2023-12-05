695fdcd4ca86fc1ad6f32a74fc9798a45a79895b
emalfroy <egon.malfroy@gmail.com>
2023-12-05 16:28:32 +0100

    refactor: remove projector legacy toggle

--- src/BuildingRegistry.Projector/Infrastructure/Modules/ApiModule.cs
+++ src/BuildingRegistry.Projector/Infrastructure/Modules/ApiModule.cs
@@ -42,7 +42,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
         private readonly IConfiguration _configuration;
         private readonly IServiceCollection _services;
         private readonly ILoggerFactory _loggerFactory;
-        private readonly bool _useProjectionsV2;
 
         public ApiModule(
             IConfiguration configuration,
@@ -52,7 +51,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
             _configuration = configuration;
             _services = services;
             _loggerFactory = loggerFactory;
-            _useProjectionsV2 = Convert.ToBoolean(_configuration.GetSection(FeatureToggleOptions.ConfigurationKey)[nameof(FeatureToggleOptions.UseProjectionsV2)]);
         }
 
         protected override void Load(ContainerBuilder builder)
@@ -81,51 +79,10 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
 
             RegisterLastChangedProjections(builder);
 
-            if (_useProjectionsV2)
-            {
-                RegisterExtractProjectionsV2(builder);
-                RegisterLegacyProjectionsV2(builder);
-                RegisterWmsProjectionsV2(builder);
-                RegisterWfsProjectionsV2(builder);
-            }
-            else
-            {
-                RegisterExtractProjections(builder);
-                RegisterLegacyProjections(builder);
-                RegisterWmsProjections(builder);
-                RegisterWfsProjections(builder);
-            }
-        }
-
-        private void RegisterExtractProjections(ContainerBuilder builder)
-        {
-            builder
-                .RegisterModule(
-                    new ExtractModule(
-                        _configuration,
-                        _services,
-                        _loggerFactory));
-
-            builder
-                .RegisterProjectionMigrator<ExtractContextMigrationFactory>(
-                    _configuration,
-                    _loggerFactory)
-
-                .RegisterProjections<BuildingExtractProjections, ExtractContext>(
-                    context =>
-                        new BuildingExtractProjections(
-                            context.Resolve<IOptions<ExtractConfig>>(),
-                            DbaseCodePage.Western_European_ANSI.ToEncoding(),
-                            WKBReaderFactory.Create()),
-                    ConnectedProjectionSettings.Default)
-
-                .RegisterProjections<BuildingUnitExtractProjections, ExtractContext>(
-                    context =>
-                        new BuildingUnitExtractProjections(
-                            context.Resolve<IOptions<ExtractConfig>>(),
-                            DbaseCodePage.Western_European_ANSI.ToEncoding(),
-                            WKBReaderFactory.Create()),
-                    ConnectedProjectionSettings.Default);
+            RegisterExtractProjectionsV2(builder);
+            RegisterLegacyProjectionsV2(builder);
+            RegisterWmsProjectionsV2(builder);
+            RegisterWfsProjectionsV2(builder);
         }
 
         private void RegisterExtractProjectionsV2(ContainerBuilder builder)
@@ -141,7 +98,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjectionMigrator<ExtractContextMigrationFactory>(
                     _configuration,
                     _loggerFactory)
-
                 .RegisterProjections<BuildingExtractV2Projections, ExtractContext>(
                     context =>
                         new BuildingExtractV2Projections(
@@ -149,7 +105,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                             DbaseCodePage.Western_European_ANSI.ToEncoding(),
                             WKBReaderFactory.Create()),
                     ConnectedProjectionSettings.Default)
-
                 .RegisterProjections<BuildingUnitExtractV2Projections, ExtractContext>(
                     context =>
                         new BuildingUnitExtractV2Projections(
@@ -157,7 +112,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                             DbaseCodePage.Western_European_ANSI.ToEncoding(),
                             WKBReaderFactory.Create()),
                     ConnectedProjectionSettings.Default)
-
                 .RegisterProjections<BuildingUnitAddressLinkExtractProjections, ExtractContext>(
                     context => new BuildingUnitAddressLinkExtractProjections(
                         context.Resolve<IOptions<ExtractConfig>>(),
@@ -185,38 +139,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjections<BuildingUnitProjections, LastChangedListContext>(ConnectedProjectionSettings.Default);
         }
 
-        private void RegisterLegacyProjections(ContainerBuilder builder)
-        {
-            builder
-                .RegisterModule(
-                    new LegacyModule(
-                        _configuration,
-                        _services,
-                        _loggerFactory));
-
-            var syndicationCatchUpSize = _configuration.GetSection(CatchUpSizesConfigKey).GetValue<int>("BuildingSyndication");
-            var syndicationProjectionSettings =
-                ConnectedProjectionSettings.Configure(settings => settings.ConfigureCatchUpPageSize(syndicationCatchUpSize));
-
-            builder
-                .RegisterProjectionMigrator<LegacyContextMigrationFactory>(
-                    _configuration,
-                    _loggerFactory)
-
-                .RegisterProjections<BuildingDetailProjections, LegacyContext>(
-                    () => new BuildingDetailProjections(),
-                    ConnectedProjectionSettings.Default)
-                .RegisterProjections<BuildingSyndicationProjections, LegacyContext>(
-                    () => new BuildingSyndicationProjections(),
-                    syndicationProjectionSettings)
-                .RegisterProjections<BuildingUnitDetailProjections, LegacyContext>(
-                    () => new BuildingUnitDetailProjections(),
-                    ConnectedProjectionSettings.Default)
-                .RegisterProjections<BuildingPersistenLocalIdCrabIdProjections, LegacyContext>(
-                    () => new BuildingPersistenLocalIdCrabIdProjections(),
-                    ConnectedProjectionSettings.Default);
-        }
-
         private void RegisterLegacyProjectionsV2(ContainerBuilder builder)
         {
             builder
@@ -234,7 +156,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjectionMigrator<LegacyContextMigrationFactory>(
                     _configuration,
                     _loggerFactory)
-
                 .RegisterProjections<BuildingDetailV2Projections, LegacyContext>(
                     () => new BuildingDetailV2Projections(),
                     ConnectedProjectionSettings.Default)
@@ -244,33 +165,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjections<BuildingUnitDetailV2Projections, LegacyContext>(
                     () => new BuildingUnitDetailV2Projections(),
                     ConnectedProjectionSettings.Default);
-        }
-
-        private void RegisterWmsProjections(ContainerBuilder builder)
-        {
-            builder
-                .RegisterModule(
-                    new WmsModule(
-                        _configuration,
-                        _services,
-                        _loggerFactory));
-
-            var wmsProjectionSettings = ConnectedProjectionSettings
-                .Configure(settings =>
-                    settings.ConfigureLinearBackoff<SqlException>(_configuration, "Wms"));
-
-            builder
-                .RegisterProjectionMigrator<WmsContextMigrationFactory>(
-                    _configuration,
-                    _loggerFactory)
-
-                .RegisterProjections<BuildingRegistry.Projections.Wms.Building.BuildingProjections, WmsContext>(() =>
-                    new BuildingRegistry.Projections.Wms.Building.BuildingProjections(WKBReaderFactory.Create()),
-                    wmsProjectionSettings)
-
-                .RegisterProjections<BuildingRegistry.Projections.Wms.BuildingUnit.BuildingUnitProjections, WmsContext>(() =>
-                    new BuildingRegistry.Projections.Wms.BuildingUnit.BuildingUnitProjections(WKBReaderFactory.Create()),
-                    wmsProjectionSettings);
         }
 
         private void RegisterWmsProjectionsV2(ContainerBuilder builder)
@@ -290,40 +184,12 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjectionMigrator<WmsContextMigrationFactory>(
                     _configuration,
                     _loggerFactory)
-
                 .RegisterProjections<BuildingRegistry.Projections.Wms.BuildingV2.BuildingV2Projections, WmsContext>(() =>
                         new BuildingRegistry.Projections.Wms.BuildingV2.BuildingV2Projections(WKBReaderFactory.Create()),
                     wmsProjectionSettings)
-
                 .RegisterProjections<BuildingRegistry.Projections.Wms.BuildingUnitV2.BuildingUnitV2Projections, WmsContext>(() =>
                         new BuildingRegistry.Projections.Wms.BuildingUnitV2.BuildingUnitV2Projections(WKBReaderFactory.Create()),
                     wmsProjectionSettings);
-        }
-
-        private void RegisterWfsProjections(ContainerBuilder builder)
-        {
-            builder
-                .RegisterModule(
-                    new WfsModule(
-                        _configuration,
-                        _services,
-                        _loggerFactory));
-
-            var wfsProjectionSettings = ConnectedProjectionSettings
-                .Configure(settings =>
-                    settings.ConfigureLinearBackoff<SqlException>(_configuration, "Wfs"));
-
-            builder
-                .RegisterProjectionMigrator<WfsContextMigrationFactory>(
-                    _configuration,
-                    _loggerFactory)
-
-                .RegisterProjections<BuildingRegistry.Projections.Wfs.Building.BuildingProjections, WfsContext>(() =>
-                        new BuildingRegistry.Projections.Wfs.Building.BuildingProjections(WKBReaderFactory.Create()),
-                    wfsProjectionSettings)
-                .RegisterProjections<BuildingRegistry.Projections.Wfs.BuildingUnit.BuildingUnitProjections, WfsContext>(() =>
-                        new BuildingRegistry.Projections.Wfs.BuildingUnit.BuildingUnitProjections(WKBReaderFactory.Create()),
-                    wfsProjectionSettings);
         }
 
         private void RegisterWfsProjectionsV2(ContainerBuilder builder)
@@ -343,7 +209,6 @@ namespace BuildingRegistry.Projector.Infrastructure.Modules
                 .RegisterProjectionMigrator<WfsContextMigrationFactory>(
                     _configuration,
                     _loggerFactory)
-
                 .RegisterProjections<BuildingRegistry.Projections.Wfs.BuildingV2.BuildingV2Projections, WfsContext>(() =>
                         new BuildingRegistry.Projections.Wfs.BuildingV2.BuildingV2Projections(WKBReaderFactory.Create()),
                     wfsProjectionSettings)
