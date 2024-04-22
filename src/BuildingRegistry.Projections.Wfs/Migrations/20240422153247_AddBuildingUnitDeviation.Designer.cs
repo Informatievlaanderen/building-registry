809d9a322260920f614ee6a03164734e4a640447
Arne Dumarey <dumarey.arne@gmail.com>
2024-04-22 17:36:52 +0200

    feat: add deviation to buildingunit WFS GAWR-5319

--- src/BuildingRegistry.Projections.Wfs/Migrations/20240422153247_AddBuildingUnitDeviation.Designer.cs
+++ src/BuildingRegistry.Projections.Wfs/Migrations/20240422153247_AddBuildingUnitDeviation.Designer.cs
@@ -4,6 +4,7 @@ using BuildingRegistry.Projections.Wfs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace BuildingRegistry.Projections.Wfs.Migrations
 {
     [DbContext(typeof(WfsContext))]
-    partial class WfsContextModelSnapshot : ModelSnapshot
+    [Migration("20240422153247_AddBuildingUnitDeviation")]
+    partial class AddBuildingUnitDeviation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
