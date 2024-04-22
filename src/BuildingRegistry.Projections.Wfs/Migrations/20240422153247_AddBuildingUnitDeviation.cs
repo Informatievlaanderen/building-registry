d9f2936a07c9a69a096803b14a6a7439927c5a39
Arne Dumarey <dumarey.arne@gmail.com>
2024-04-22 17:36:52 +0200

    feat: add deviation to buildingunit WFS GAWR-5319

---
+++ src/BuildingRegistry.Projections.Wfs/Migrations/20240422153247_AddBuildingUnitDeviation.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingRegistry.Projections.Wfs.Migrations
+{
+    using Infrastructure;
+
+    /// <inheritdoc />
+    public partial class AddBuildingUnitDeviation : Migration
+    {
+        private const string BuildingUnitViewName = "GebouweenheidView";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW [{Schema.Wfs}].[{BuildingUnitViewName}]");
+            migrationBuilder.Sql($@"
+                CREATE VIEW [{Schema.Wfs}].[{BuildingUnitViewName}]
+                WITH SCHEMABINDING
+                AS
+                SELECT
+                    [BuildingUnitPersistentLocalId] AS [MyId],
+                    [Id],
+                    [BuildingUnitPersistentLocalId] AS [ObjectId],
+                    [VersionAsString] AS [VersieId],
+                    [Position] AS [Geometrie],
+                    [PositionMethod] AS [PositieGeometrieMethode],
+                    [Status] AS [GebouweenheidStatus],
+                    [Function] AS [Functie],
+                    [HasDeviation] As [AfwijkingVastgesteld],
+                    [BuildingPersistentLocalId] AS [GebouwObjectId]
+                FROM [{Schema.Wfs}].[{BuildingUnitV2.BuildingUnitConfiguration.TableName}]
+                WHERE [IsRemoved] = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW [{Schema.Wfs}].[{BuildingUnitViewName}]");
+            migrationBuilder.Sql($@"
+                CREATE VIEW [{Schema.Wfs}].[{BuildingUnitViewName}]
+                WITH SCHEMABINDING
+                AS
+                SELECT
+                    [BuildingUnitPersistentLocalId] AS [MyId],
+                    [Id],
+                    [BuildingUnitPersistentLocalId] AS [ObjectId],
+                    [VersionAsString] AS [VersieId],
+                    [Position] AS [Geometrie],
+                    [PositionMethod] AS [PositieGeometrieMethode],
+                    [Status] AS [GebouweenheidStatus],
+                    [Function] AS [Functie],
+                    [BuildingPersistentLocalId] AS [GebouwObjectId]
+                FROM [{Schema.Wfs}].[{BuildingUnitV2.BuildingUnitConfiguration.TableName}]
+                WHERE [IsRemoved] = 0");
+        }
+    }
+}
