29756580d0245e30dd9ac5f39c04244f8e2b5040
jvandaal <jonas.vandaal@outlook.be>
2024-05-21 15:26:07 +0200

    chore: add ef migrations for legacy context

--- src/BuildingRegistry.Projections.Legacy/Migrations/20240521132459_AddCountOnParcelAddress.Designer.cs
+++ src/BuildingRegistry.Projections.Legacy/Migrations/20240521132459_AddCountOnParcelAddress.Designer.cs
@@ -4,6 +4,7 @@ using BuildingRegistry.Projections.Legacy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
@@ -12,9 +13,11 @@ using NetTopologySuite.Geometries;
 namespace BuildingRegistry.Projections.Legacy.Migrations
 {
     [DbContext(typeof(LegacyContext))]
-    partial class LegacyContextModelSnapshot : ModelSnapshot
+    [Migration("20240521132459_AddCountOnParcelAddress")]
+    partial class AddCountOnParcelAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
