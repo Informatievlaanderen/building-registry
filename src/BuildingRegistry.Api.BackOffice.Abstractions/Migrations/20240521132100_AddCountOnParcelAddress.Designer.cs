1e8d3ec4f233b186227ededf2c7e25db6c711ba4
jvandaal <jonas.vandaal@outlook.be>
2024-05-21 15:22:05 +0200

    chore: add ef migrations for backoffice context

--- src/BuildingRegistry.Api.BackOffice.Abstractions/Migrations/20240521132100_AddCountOnParcelAddress.Designer.cs
+++ src/BuildingRegistry.Api.BackOffice.Abstractions/Migrations/20240521132100_AddCountOnParcelAddress.Designer.cs
@@ -3,6 +3,7 @@ using BuildingRegistry.Api.BackOffice.Abstractions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BuildingRegistry.Api.BackOffice.Abstractions.Migrations
 {
     [DbContext(typeof(BackOfficeContext))]
-    partial class BackOfficeContextModelSnapshot : ModelSnapshot
+    [Migration("20240521132100_AddCountOnParcelAddress")]
+    partial class AddCountOnParcelAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
