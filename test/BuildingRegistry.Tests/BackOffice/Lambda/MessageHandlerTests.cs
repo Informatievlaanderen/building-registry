f8fc1c0e3679177dc440160c99955424312228ed
Rik De Peuter <rikdepeuter@gmail.com>
2024-05-28 09:39:18 +0200

    add tests

--- test/BuildingRegistry.Tests/BackOffice/Lambda/MessageHandlerTests.cs
+++ test/BuildingRegistry.Tests/BackOffice/Lambda/MessageHandlerTests.cs
@@ -955,6 +955,38 @@ namespace BuildingRegistry.Tests.BackOffice.Lambda
                             request.DateTimeStatusChange == messageData.DateTimeStatusChange),
                         It.IsAny<CancellationToken>()), Times.Once);
         }
+
+        [Fact]
+        public async Task WhenProcessingCreateOsloSnapshotsSqsRequest_ThenCreateOsloSnapshotsLambdaRequestIsSent()
+        {
+            // Arrange
+            var mediator = new Mock<IMediator>();
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.Register(_ => mediator.Object);
+            var container = containerBuilder.Build();
+
+            var messageData = Fixture.Create<CreateOsloSnapshotsSqsRequest>();
+            var messageMetadata = new MessageMetadata { MessageGroupId = Fixture.Create<string>() };
+
+            var sut = new MessageHandler(container);
+
+            // Act
+            await sut.HandleMessage(
+                messageData,
+                messageMetadata,
+                CancellationToken.None);
+
+            // Assert
+            mediator
+                .Verify(x => x.Send(It.Is<CreateOsloSnapshotsLambdaRequest>(request =>
+                    request.TicketId == messageData.TicketId &&
+                    request.MessageGroupId == messageMetadata.MessageGroupId &&
+                    request.Request == messageData.Request &&
+                    string.IsNullOrEmpty(request.IfMatchHeaderValue) &&
+                    request.Provenance == messageData.ProvenanceData.ToProvenance() &&
+                    request.Metadata == messageData.Metadata
+                ), It.IsAny<CancellationToken>()), Times.Once);
+        }
     }
 
     internal class TestSqsRequest : SqsRequest
---
+++ test/BuildingRegistry.Tests/BackOffice/Sqs/BuildingUnit/GivenCreateOsloSnapshotsBackOfficeRequest.cs
@@ -0,0 +1,70 @@
+namespace BuildingRegistry.Tests.BackOffice.Sqs.BuildingUnit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AllStream;
+    using AutoFixture;
+    using Be.Vlaanderen.Basisregisters.MessageHandling.AwsSqs.Simple;
+    using Be.Vlaanderen.Basisregisters.Sqs;
+    using BuildingRegistry.Api.BackOffice.Abstractions.BuildingUnit.Requests;
+    using BuildingRegistry.Api.BackOffice.Abstractions.BuildingUnit.SqsRequests;
+    using BuildingRegistry.Api.BackOffice.Handlers.BuildingUnit;
+    using BuildingRegistry.Building;
+    using Fixtures;
+    using FluentAssertions;
+    using Moq;
+    using TicketingService.Abstractions;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class GivenCreateOsloSnapshotsBackOfficeRequest : BuildingRegistryTest
+    {
+        public GivenCreateOsloSnapshotsBackOfficeRequest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            Fixture.Customize(new WithFixedBuildingPersistentLocalId());
+            Fixture.Customize(new WithFixedBuildingUnitPersistentLocalId());
+        }
+
+        [Fact]
+        public async Task ThenTicketWithLocationIsCreated()
+        {
+            // Arrange
+            var ticketId = Fixture.Create<Guid>();
+            var ticketingMock = new Mock<ITicketing>();
+            ticketingMock
+                .Setup(x => x.CreateTicket(It.IsAny<IDictionary<string, string>>(), CancellationToken.None))
+                .ReturnsAsync(ticketId);
+
+            var ticketingUrl = new TicketingUrl(Fixture.Create<Uri>().ToString());
+
+            var sqsQueue = new Mock<ISqsQueue>();
+
+            var sut = new CreateOsloSnapshotsSqsHandler(
+                sqsQueue.Object,
+                ticketingMock.Object,
+                ticketingUrl);
+
+            var sqsRequest = new CreateOsloSnapshotsSqsRequest
+            {
+                Request = new CreateOsloSnapshotsRequest
+                {
+                    BuildingUnitPersistentLocalIds = Fixture.CreateMany<BuildingUnitPersistentLocalId>().Select(x => (int)x).ToList()
+                }
+            };
+
+            // Act
+            var result = await sut.Handle(sqsRequest, CancellationToken.None);
+
+            // Assert
+            sqsRequest.TicketId.Should().Be(ticketId);
+            sqsQueue.Verify(x => x.Copy(
+                sqsRequest,
+                It.Is<SqsQueueOptions>(y => y.MessageGroupId == AllStreamId.Instance.ToString()),
+                CancellationToken.None));
+            result.Location.Should().Be(ticketingUrl.For(ticketId));
+        }
+    }
+}
