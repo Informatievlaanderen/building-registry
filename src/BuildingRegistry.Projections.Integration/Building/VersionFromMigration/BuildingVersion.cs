6056e9b95388bf8e16b6666d7e3546ce94202634
jvandaal <jonas.vandaal@outlook.be>
2024-05-28 21:12:04 +0200

    feat: add integration version projections from migration

---
+++ src/BuildingRegistry.Projections.Integration/Building/VersionFromMigration/BuildingVersion.cs
@@ -0,0 +1,165 @@
+﻿namespace BuildingRegistry.Projections.Integration.Building.VersionFromMigration
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using Be.Vlaanderen.Basisregisters.GrAr.Common;
+    using Be.Vlaanderen.Basisregisters.Utilities;
+    using BuildingRegistry.Infrastructure;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using NetTopologySuite.Geometries;
+    using NodaTime;
+
+    public sealed class BuildingVersion
+    {
+        public const string VersionTimestampBackingPropertyName = nameof(VersionTimestampAsDateTimeOffset);
+        public const string CreatedOnTimestampBackingPropertyName = nameof(CreatedOnTimestampAsDateTimeOffset);
+        public const string LastChangedOnTimeStampBackingPropertyName = nameof(LastChangedOnAsDateTimeOffset);
+
+        public long Position { get; set; }
+
+        public int BuildingPersistentLocalId { get; set; }
+        public string Status { get; set; }
+        public string OsloStatus { get; set; }
+        public string Type { get; set; }
+        public string GeometryMethod { get; set; }
+        public string OsloGeometryMethod { get; set; }
+        public Geometry Geometry { get; set; }
+        public bool IsRemoved { get; set; }
+
+        public string PuriId { get; set; }
+        public string Namespace { get; set; }
+
+        public string VersionAsString { get; set; }
+        private DateTimeOffset VersionTimestampAsDateTimeOffset { get; set; }
+
+        public Instant VersionTimestamp
+        {
+            get => Instant.FromDateTimeOffset(VersionTimestampAsDateTimeOffset);
+            set
+            {
+                VersionTimestampAsDateTimeOffset = value.ToDateTimeOffset();
+                VersionAsString = new Rfc3339SerializableDateTimeOffset(value.ToBelgianDateTimeOffset()).ToString();
+            }
+        }
+
+        public string CreatedOnAsString { get; set; }
+        private DateTimeOffset CreatedOnTimestampAsDateTimeOffset { get; set; }
+
+        public Instant CreatedOnTimestamp
+        {
+            get => Instant.FromDateTimeOffset(CreatedOnTimestampAsDateTimeOffset);
+            set
+            {
+                CreatedOnTimestampAsDateTimeOffset = value.ToDateTimeOffset();
+                CreatedOnAsString = new Rfc3339SerializableDateTimeOffset(value.ToBelgianDateTimeOffset()).ToString();
+            }
+        }
+
+        public string LastChangedOnAsString { get; set; }
+        private DateTimeOffset LastChangedOnAsDateTimeOffset { get; set; }
+
+        public Instant LastChangedOnTimestamp
+        {
+            get => Instant.FromDateTimeOffset(LastChangedOnAsDateTimeOffset);
+            set
+            {
+                LastChangedOnAsDateTimeOffset = value.ToDateTimeOffset();
+                LastChangedOnAsString = new Rfc3339SerializableDateTimeOffset(value.ToBelgianDateTimeOffset()).ToString();
+            }
+        }
+
+        public Collection<BuildingUnitVersion> BuildingUnits { get; set; }
+
+        public BuildingVersion()
+        {
+            BuildingUnits = new Collection<BuildingUnitVersion>();
+        }
+
+        public BuildingVersion CloneAndApplyEventInfo(long newPosition,
+            string eventName,
+            Instant lastChangedOn,
+            Action<BuildingVersion> editFunc)
+        {
+            var buildingUnits =
+                BuildingUnits.Select(x => x.CloneAndApplyEventInfo(newPosition, eventName));
+
+            var newItem = new BuildingVersion
+            {
+                Position = newPosition,
+
+                BuildingPersistentLocalId = BuildingPersistentLocalId,
+                Status = Status,
+                OsloStatus = OsloStatus,
+                Type = eventName,
+                GeometryMethod = GeometryMethod,
+                OsloGeometryMethod = OsloGeometryMethod,
+                Geometry = Geometry,
+                IsRemoved = IsRemoved,
+
+                PuriId = PuriId,
+                Namespace = Namespace,
+
+                VersionTimestamp = VersionTimestamp,
+                CreatedOnTimestamp = CreatedOnTimestamp,
+                LastChangedOnTimestamp = lastChangedOn,
+
+                BuildingUnits = new Collection<BuildingUnitVersion>(buildingUnits.ToList()),
+            };
+
+            editFunc(newItem);
+
+            return newItem;
+        }
+    }
+
+    public sealed class BuildingLatestEventConfiguration : IEntityTypeConfiguration<BuildingVersion>
+    {
+        public void Configure(EntityTypeBuilder<BuildingVersion> builder)
+        {
+            const string tableName = "building_versions_migration";
+
+            builder
+                .ToTable(tableName, Schema.Integration) // to schema per type
+                .HasKey(x => x.Position);
+
+            builder.Property(x => x.Position).ValueGeneratedNever();
+
+            builder.Property(x => x.Position).HasColumnName("position");
+            builder.Property(x => x.BuildingPersistentLocalId).HasColumnName("building_persistent_local_id");
+            builder.Property(x => x.Status).HasColumnName("status");
+            builder.Property(x => x.OsloStatus).HasColumnName("oslo_status");
+            builder.Property(x => x.Type).HasColumnName("type");
+            builder.Property(x => x.GeometryMethod).HasColumnName("geometry_method");
+            builder.Property(x => x.OsloGeometryMethod).HasColumnName("oslo_geometry_method");
+            builder.Property(x => x.Geometry).HasColumnName("geometry");
+            builder.Property(x => x.IsRemoved).HasColumnName("is_removed");
+            builder.Property(x => x.PuriId).HasColumnName("puri_id");
+            builder.Property(x => x.Namespace).HasColumnName("namespace");
+            builder.Property(x => x.VersionAsString).HasColumnName("version_as_string");
+            builder.Property(BuildingVersion.VersionTimestampBackingPropertyName).HasColumnName("version_timestamp");
+            builder.Property(x => x.CreatedOnAsString).HasColumnName("created_on_as_string");
+            builder.Property(BuildingVersion.CreatedOnTimestampBackingPropertyName).HasColumnName("created_on_timestamp");
+            builder.Property(x => x.LastChangedOnAsString).HasColumnName("last_changed_on_as_string");
+            builder.Property(BuildingVersion.LastChangedOnTimeStampBackingPropertyName).HasColumnName("last_changed_on_timestamp");
+
+            builder.HasMany(x => x.BuildingUnits)
+                .WithOne()
+                .HasForeignKey(x => x.Position)
+                .IsRequired();
+
+            builder.Ignore(x => x.VersionTimestamp);
+            builder.Ignore(x => x.CreatedOnTimestamp);
+            builder.Ignore(x => x.LastChangedOnTimestamp);
+
+            builder.HasIndex(x => x.BuildingPersistentLocalId);
+            builder.HasIndex(x => x.Status);
+            builder.HasIndex(x => x.OsloStatus);
+            builder.HasIndex(x => x.Type);
+            builder.HasIndex(x => x.IsRemoved);
+            builder.HasIndex(x => x.Geometry).HasMethod("GIST");
+            builder.HasIndex(BuildingVersion.VersionTimestampBackingPropertyName);
+        }
+    }
+}
---
+++ src/BuildingRegistry.Projections.Integration/Migrations/20240528191749_AddVersionsFromMigration.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+#nullable disable
+
+namespace BuildingRegistry.Projections.Integration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVersionsFromMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "building_versions_migration",
+                schema: "integration_building",
+                columns: table => new
+                {
+                    position = table.Column<long>(type: "bigint", nullable: false),
+                    building_persistent_local_id = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    oslo_status = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    geometry_method = table.Column<string>(type: "text", nullable: false),
+                    oslo_geometry_method = table.Column<string>(type: "text", nullable: false),
+                    geometry = table.Column<Geometry>(type: "geometry", nullable: false),
+                    is_removed = table.Column<bool>(type: "boolean", nullable: false),
+                    puri_id = table.Column<string>(type: "text", nullable: false),
+                    @namespace = table.Column<string>(name: "namespace", type: "text", nullable: false),
+                    version_as_string = table.Column<string>(type: "text", nullable: false),
+                    version_timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_on_as_string = table.Column<string>(type: "text", nullable: false),
+                    created_on_timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    last_changed_on_as_string = table.Column<string>(type: "text", nullable: false),
+                    last_changed_on_timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_building_versions_migration", x => x.position);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "building_unit_versions_migration",
+                schema: "integration_building",
+                columns: table => new
+                {
+                    position = table.Column<long>(type: "bigint", nullable: false),
+                    building_unit_persistent_local_id = table.Column<int>(type: "integer", nullable: false),
+                    building_persistent_local_id = table.Column<int>(type: "integer", nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    oslo_status = table.Column<string>(type: "text", nullable: false),
+                    function = table.Column<string>(type: "text", nullable: false),
+                    oslo_function = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    geometry_method = table.Column<string>(type: "text", nullable: false),
+                    oslo_geometry_method = table.Column<string>(type: "text", nullable: false),
+                    geometry = table.Column<Geometry>(type: "geometry", nullable: false),
+                    has_deviation = table.Column<bool>(type: "boolean", nullable: false),
+                    is_removed = table.Column<bool>(type: "boolean", nullable: false),
+                    puri_id = table.Column<string>(type: "text", nullable: false),
+                    @namespace = table.Column<string>(name: "namespace", type: "text", nullable: false),
+                    version_as_string = table.Column<string>(type: "text", nullable: false),
+                    version_timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    created_on_as_string = table.Column<string>(type: "text", nullable: false),
+                    created_on_timestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_building_unit_versions_migration", x => new { x.position, x.building_unit_persistent_local_id });
+                    table.ForeignKey(
+                        name: "FK_building_unit_versions_migration_building_versions_migratio~",
+                        column: x => x.position,
+                        principalSchema: "integration_building",
+                        principalTable: "building_versions_migration",
+                        principalColumn: "position",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "building_unit_address_versions_migration",
+                schema: "integration_building",
+                columns: table => new
+                {
+                    position = table.Column<long>(type: "bigint", nullable: false),
+                    building_unit_persistent_local_id = table.Column<int>(type: "integer", nullable: false),
+                    address_persistent_local_id = table.Column<int>(type: "integer", nullable: false),
+                    count = table.Column<int>(type: "integer", nullable: false, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_building_unit_address_versions_migration", x => new { x.position, x.building_unit_persistent_local_id, x.address_persistent_local_id });
+                    table.ForeignKey(
+                        name: "FK_building_unit_address_versions_migration_building_unit_vers~",
+                        columns: x => new { x.position, x.building_unit_persistent_local_id },
+                        principalSchema: "integration_building",
+                        principalTable: "building_unit_versions_migration",
+                        principalColumns: new[] { "position", "building_unit_persistent_local_id" },
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_address_versions_migration_address_persistent~",
+                schema: "integration_building",
+                table: "building_unit_address_versions_migration",
+                column: "address_persistent_local_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_address_versions_migration_building_unit_pers~",
+                schema: "integration_building",
+                table: "building_unit_address_versions_migration",
+                column: "building_unit_persistent_local_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_address_versions_migration_position",
+                schema: "integration_building",
+                table: "building_unit_address_versions_migration",
+                column: "position");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_building_persistent_local_~",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "building_persistent_local_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_building_unit_persistent_l~",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "building_unit_persistent_local_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_geometry",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "geometry")
+                .Annotation("Npgsql:IndexMethod", "GIST");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_is_removed",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "is_removed");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_oslo_status",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "oslo_status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_status",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_type",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "type");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_unit_versions_migration_version_timestamp",
+                schema: "integration_building",
+                table: "building_unit_versions_migration",
+                column: "version_timestamp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_building_persistent_local_id",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "building_persistent_local_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_geometry",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "geometry")
+                .Annotation("Npgsql:IndexMethod", "GIST");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_is_removed",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "is_removed");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_oslo_status",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "oslo_status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_status",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_type",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "type");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_migration_version_timestamp",
+                schema: "integration_building",
+                table: "building_versions_migration",
+                column: "version_timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "building_unit_address_versions_migration",
+                schema: "integration_building");
+
+            migrationBuilder.DropTable(
+                name: "building_unit_versions_migration",
+                schema: "integration_building");
+
+            migrationBuilder.DropTable(
+                name: "building_versions_migration",
+                schema: "integration_building");
+        }
+    }
+}
