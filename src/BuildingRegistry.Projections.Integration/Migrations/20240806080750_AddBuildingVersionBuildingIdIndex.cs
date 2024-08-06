af3c4f50ef809552230cfe833a6d009254206bed
Rik De Peuter <rikdepeuter@gmail.com>
2024-08-06 10:08:14 +0200

    fix: add migration

---
+++ src/BuildingRegistry.Projections.Integration/Migrations/20240806080750_AddBuildingVersionBuildingIdIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingRegistry.Projections.Integration.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuildingVersionBuildingIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_building_versions_building_id",
+                schema: "integration_building",
+                table: "building_versions",
+                column: "building_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_building_versions_building_id",
+                schema: "integration_building",
+                table: "building_versions");
+        }
+    }
+}
