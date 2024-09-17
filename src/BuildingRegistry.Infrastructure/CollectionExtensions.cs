3e51b8d56e14c3a00514680d7004e0418cc993b9
Rik De Peuter <rikdepeuter@gmail.com>
2024-09-17 11:48:35 +0200

    chore: add CacheInvalidator host

---
+++ src/BuildingRegistry.Infrastructure/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace BuildingRegistry.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<ICollection<T>> SplitIntoBatches<T>(this ICollection<T> collection, int batchCount)
+        {
+            var batchSize = (double)collection.Count / batchCount;
+            var batchSizeRounded = (int)Math.Floor(batchSize);
+            if (batchSize != batchSizeRounded)
+            {
+                batchSizeRounded++;
+            }
+            return Enumerable.Range(0, batchCount).Select(batchIndex => collection.Skip(batchSizeRounded * batchIndex).Take(batchSizeRounded).ToArray());
+        }
+    }
+}
---
+++ test/BuildingRegistry.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace BuildingRegistry.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Infrastructure;
+    using Xunit;
+
+    public class CollectionExtensionsTests
+    {
+        [Theory]
+        [MemberData(nameof(SplitIntoBatchesCases))]
+        public void SplitIntoBatches(int collectionCount, int batchCount, int[] expectedBatchSizes)
+        {
+            var collection = Enumerable.Range(1, collectionCount).ToArray();
+            var batches = collection.SplitIntoBatches(batchCount).ToArray();
+            Assert.Equal(expectedBatchSizes.Length, batches.Length);
+            for (var i = 0; i < batches.Length; i++)
+            {
+                Assert.Equal(expectedBatchSizes[i], batches[i].Count);
+            }
+        }
+
+        public static IEnumerable<object[]> SplitIntoBatchesCases()
+        {
+            yield return new object[] { 1, 1, new[] { 1 } };
+            yield return new object[] { 2, 1, new[] { 2 } };
+            yield return new object[] { 1, 2, new[] { 1, 0 } };
+            yield return new object[] { 2, 2, new[] { 1, 1 } };
+            yield return new object[] { 3, 2, new[] { 2, 1 } };
+        }
+    }
+}
