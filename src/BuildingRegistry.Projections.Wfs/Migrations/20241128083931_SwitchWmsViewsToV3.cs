29596ffc0e6dbea49ae65bf534ba32526f108a26
ArneD <dumarey.arne@gmail.com>
2024-11-28 10:12:57 +0100

    feat: switch wms/wfs views V3 GAWR-6685

---
+++ src/BuildingRegistry.Projections.Wfs/Migrations/20241128083931_SwitchWmsViewsToV3.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingRegistry.Projections.Wfs.Migrations
+{
+    using Infrastructure;
+
+    /// <inheritdoc />
+    public partial class SwitchWmsViewsToV3 : Migration
+    {
+        private const string BuildingViewName = "GebouwView";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW [{Schema.Wfs}].[{BuildingViewName}]");
+
+            migrationBuilder.Sql($@"
+                CREATE VIEW [{Schema.Wfs}].[{BuildingViewName}]
+                WITH SCHEMABINDING
+                AS
+                SELECT
+                    [PersistentLocalId] AS [MyId],
+                    [Id],
+                    [PersistentLocalId] AS [ObjectId],
+                    [VersionAsString] AS [VersieId],
+                    [Geometry] AS [Geometrie],
+                    [GeometryMethod] AS [GeometrieMethode],
+                    [Status] AS [GebouwStatus]
+                FROM [{Schema.Wfs}].[{BuildingV3.BuildingConfiguration.TableName}]
+                WHERE [IsRemoved] = 0 and Geometry is not null");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"DROP VIEW [{Schema.Wfs}].[{BuildingViewName}]");
+
+            migrationBuilder.Sql($@"
+                CREATE VIEW [{Schema.Wfs}].[{BuildingViewName}]
+                WITH SCHEMABINDING
+                AS
+                SELECT
+                    [PersistentLocalId] AS [MyId],
+                    [Id],
+                    [PersistentLocalId] AS [ObjectId],
+                    [VersionAsString] AS [VersieId],
+                    [Geometry] AS [Geometrie],
+                    [GeometryMethod] AS [GeometrieMethode],
+                    [Status] AS [GebouwStatus]
+                FROM [{Schema.Wfs}].[{BuildingV2.BuildingConfiguration.TableName}]
+                WHERE [IsRemoved] = 0 and Geometry is not null");
+        }
+    }
+}
