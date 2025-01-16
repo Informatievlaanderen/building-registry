f51d1edf35273c67529d6846d6c852743eddf3b5
Arne Dumarey <dumarey.arne@gmail.com>
2025-01-16 16:34:32 +0100

    refactor(producers): use projector endpoints

--- src/BuildingRegistry.Producer/Infrastructure/Startup.cs
+++ src/BuildingRegistry.Producer/Infrastructure/Startup.cs
@@ -1,17 +1,11 @@
 ﻿namespace BuildingRegistry.Producer.Infrastructure
 {
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading;
     using Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Json;
-    using Be.Vlaanderen.Basisregisters.Projector.ConnectedProjections;
-    using Be.Vlaanderen.Basisregisters.Projector.Controllers;
+    using Be.Vlaanderen.Basisregisters.Projector;
     using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Http;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Newtonsoft.Json;
-    using SqlStreamStore;
 
     public class Startup
     {
@@ -22,44 +16,9 @@
 
             app.UseHealthChecks("/health");
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("v1/projections", async context =>
-                {
-                    var configuration = app.ApplicationServices.GetRequiredService<IConfiguration>();
-                    var manager = app.ApplicationServices.GetRequiredService<IConnectedProjectionsManager>();
-                    var streamStore = app.ApplicationServices.GetRequiredService<IStreamStore>();
-
-                    var baseUri = configuration.GetValue<string>("BaseUrl").TrimEnd('/');
-
-                    var registeredConnectedProjections = manager
-                        .GetRegisteredProjections()
-                        .ToList();
-                    var projectionStates = await manager.GetProjectionStates(CancellationToken.None);
-                    var responses = registeredConnectedProjections.Aggregate(
-                        new List<ProjectionResponse>(),
-                        (list, projection) =>
-                        {
-                            var projectionState = projectionStates.SingleOrDefault(x => x.Name == projection.Id);
-                            list.Add(new ProjectionResponse(
-                                projection,
-                                projectionState,
-                                baseUri));
-                            return list;
-                        });
-
-                    var streamPosition = await streamStore.ReadHeadPosition();
-
-                    var projectionResponseList = new ProjectionResponseList(responses, baseUri)
-                    {
-                        StreamPosition = streamPosition
-                    };
-
-                    var json = JsonConvert.SerializeObject(projectionResponseList, new JsonSerializerSettings().ConfigureDefaultForApi());
-
-                    await context.Response.WriteAsync(json);
-                });
-            });
+            var configuration = app.ApplicationServices.GetRequiredService<IConfiguration>();
+            var baseUri = configuration.GetValue<string>("BaseUrl").TrimEnd('/');
+            app.UseProjectorEndpoints(baseUri, new JsonSerializerSettings().ConfigureDefaultForApi());
         }
     }
 }
