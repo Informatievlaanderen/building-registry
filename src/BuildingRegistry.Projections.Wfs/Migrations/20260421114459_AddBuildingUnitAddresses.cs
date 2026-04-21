8d397297f2cd69c11189d4867dadfe0ba4179c63
Arne Dumarey <dumarey.arne@gmail.com>
2026-04-21 14:04:30 +0200

    feat(wfs): add buildingunit addresses

---
+++ src/BuildingRegistry.Projections.Wfs/Migrations/20260421114459_AddBuildingUnitAddresses.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildingRegistry.Projections.Wfs.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuildingUnitAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BuildingUnitAddresses",
+                schema: "wfs",
+                columns: table => new
+                {
+                    BuildingUnitPersistentLocalId = table.Column<int>(type: "int", nullable: false),
+                    AddressPersistentLocalId = table.Column<int>(type: "int", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BuildingUnitAddresses", x => new { x.BuildingUnitPersistentLocalId, x.AddressPersistentLocalId });
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuildingUnitAddresses_AddressPersistentLocalId",
+                schema: "wfs",
+                table: "BuildingUnitAddresses",
+                column: "AddressPersistentLocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BuildingUnitAddresses_BuildingUnitPersistentLocalId",
+                schema: "wfs",
+                table: "BuildingUnitAddresses",
+                column: "BuildingUnitPersistentLocalId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BuildingUnitAddresses",
+                schema: "wfs");
+        }
+    }
+}
