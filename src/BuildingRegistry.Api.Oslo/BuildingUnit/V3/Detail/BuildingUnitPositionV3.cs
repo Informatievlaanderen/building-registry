ca96c4ab1f7e8304737031b9f0b5e4508284dd6f
ArneD <dumarey.arne@gmail.com>
2026-08-20 10:19:32 +0200

    fix(oslo): buildingunit geometriemethod to structured obj

--- src/BuildingRegistry.Api.Oslo/BuildingUnit/V3/Detail/BuildingUnitPositionV3.cs
+++ src/BuildingRegistry.Api.Oslo/BuildingUnit/V3/Detail/BuildingUnitPositionV3.cs
@@ -4,6 +4,7 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.V3.Detail
     using Be.Vlaanderen.Basisregisters.GrAr.Oslo;
     using Be.Vlaanderen.Basisregisters.GrAr.Oslo.Gml;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
 
     /// <summary>
     /// De geometrie van het object in gml-formaat.
@@ -20,18 +21,50 @@ namespace BuildingRegistry.Api.Oslo.BuildingUnit.V3.Detail
         /// De geometriemethode van de gebouweenheidpositie.
         /// </summary>
         [JsonProperty(PropertyName = "methode", Order = 1, Required = Required.DisallowNull)]
-        public PositieGeometrieMethode GeometryMethod { get; set; }
+        public GebouweenheidPositieGeometrieMethodeV3 GeometryMethod { get; set; }
 
         public BuildingUnitPositionV3(List<PointGeometrie> geometries, PositieGeometrieMethode geometryMethod)
         {
             Geometry = geometries;
-            GeometryMethod = geometryMethod;
+            GeometryMethod = new GebouweenheidPositieGeometrieMethodeV3(geometryMethod);
         }
 
         public BuildingUnitPositionV3(List<string> gmlGeometries, PositieGeometrieMethode geometryMethod)
         {
             Geometry = gmlGeometries.ConvertAll(gml => new PointGeometrie(gml));
-            GeometryMethod = geometryMethod;
+            GeometryMethod = new GebouweenheidPositieGeometrieMethodeV3(geometryMethod);
+        }
+    }
+
+    /// <summary>
+    /// De gebruikte methode om de positie te bepalen.
+    /// </summary>
+    public class GebouweenheidPositieGeometrieMethodeV3
+    {
+        private static readonly CamelCaseNamingStrategy NamingStrategy = new();
+
+        /// <summary>
+        /// Identificatie van de methode.
+        /// </summary>
+        [JsonProperty("@id", Required = Required.DisallowNull, Order = 1)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Linked data type van het object.
+        /// </summary>
+        [JsonProperty("@type", Required = Required.DisallowNull, Order = 2)]
+        public string Type => "Concept";
+
+        /// <summary>
+        /// De beschrijving van de methode.
+        /// </summary>
+        [JsonProperty("code", Required = Required.DisallowNull, Order = 3)]
+        public PositieGeometrieMethode Label { get; set; }
+
+        public GebouweenheidPositieGeometrieMethodeV3(PositieGeometrieMethode positieGeometrieMethode)
+        {
+            Label = positieGeometrieMethode;
+            Id = OsloNamespaces.AdresGeometrieMethode.ToPuri(NamingStrategy.GetPropertyName(positieGeometrieMethode.ToString(), false));
         }
     }
 }
